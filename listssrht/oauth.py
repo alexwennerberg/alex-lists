b2c45232d262a6204a493a1d8284ab88352ac6b7
Drew DeVault <sir@cmpwn.com>
2019-04-11 10:45:45 -0400

    Update email subscriptions to user_id on login

--- listssrht/oauth.py
+++ listssrht/oauth.py
@@ -1,6 +1,7 @@
 from srht.config import cfg
+from srht.database import db
 from srht.oauth import AbstractOAuthService, DelegatedScope
-from listssrht.types import User, OAuthToken
+from listssrht.types import User, OAuthToken, Subscription
 
 client_id = cfg("lists.sr.ht", "oauth-client-id")
 client_secret = cfg("lists.sr.ht", "oauth-client-secret")
@@ -12,3 +13,14 @@ class ListsOAuthService(AbstractOAuthService):
             DelegatedScope("email", "emails", False),
             DelegatedScope("subs", "subscriptions", True),
         ], user_class=User, token_class=OAuthToken)
+
+    def lookup_or_register(self, token, token_expires, scopes):
+        user = super().lookup_or_register(token, token_expires, scopes)
+        db.session.flush()
+        # Rewire existing subscriptions
+        for sub in Subscription.query.filter(
+                Subscription.email == user.email).all():
+            sub.email = None
+            sub.user_id = user.id
+        db.session.commit()
+        return user
