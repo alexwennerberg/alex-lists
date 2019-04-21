6928c6e56b96ef93ba94bbe5528716ac4109e17e
Drew DeVault <sir@cmpwn.com>
2019-04-21 12:14:20 -0400

    Adopt existing emails on login and profile update

--- listssrht/oauth.py
+++ listssrht/oauth.py
@@ -1,7 +1,9 @@
+from email.utils import parseaddr
 from srht.config import cfg
 from srht.database import db
 from srht.oauth import AbstractOAuthService, DelegatedScope
-from listssrht.types import User, OAuthToken, Subscription
+from listssrht.types import Email, OAuthToken, Subscription, User
+import sqlalchemy as sa
 
 client_id = cfg("lists.sr.ht", "oauth-client-id")
 client_secret = cfg("lists.sr.ht", "oauth-client-secret")
@@ -16,11 +18,32 @@ class ListsOAuthService(AbstractOAuthService):
 
     def lookup_or_register(self, token, token_expires, scopes):
         user = super().lookup_or_register(token, token_expires, scopes)
-        db.session.flush()
-        # Rewire existing subscriptions
-        for sub in Subscription.query.filter(
-                Subscription.email == user.email).all():
-            sub.email = None
-            sub.user_id = user.id
+        if not user.id:
+            db.session.flush()
+            # Rewire existing subscriptions
+            for sub in Subscription.query.filter(
+                    Subscription.email == user.email).all():
+                sub.email = None
+                sub.user_id = user.id
+            # Rewire existing emails
+            for email in (Email.query
+                    .filter(Email.sender_id is None)
+                    .filter(sa.cast(Email.headers["From"], sa.String)
+                        .ilike("%" + user.email + "%"))):
+                name, addr = parseaddr(email.headers["From"])
+                if addr == user.email:
+                    email.sender_id = user.id
         db.session.commit()
         return user
+
+    def profile_update_hook(self, user, payload):
+        if user.email != payload["email"]:
+            # Adopt emails sent with this address
+            for email in (Email.query
+                    .filter(Email.sender_id is None)
+                    .filter(sa.cast(Email.headers["From"], sa.String)
+                        .ilike("%" + user.email + "%"))):
+                name, addr = parseaddr(email.headers["From"])
+                if addr == payload["email"]:
+                    email.sender_id = user.id
+        super().profile_update_hook(user, payload)
