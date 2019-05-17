9ab01a2eb4c82f97587263b8dbbc19142542e2f6
Drew DeVault <sir@cmpwn.com>
2019-05-17 10:16:12 -0400

    Use email.message.as_string for encoding mail

--- listssrht/process.py
+++ listssrht/process.py
@@ -70,7 +70,7 @@ def _forward(dest, mail):
             print(to + " is already copied, skipping")
             continue
         print("Forwarding message to " + to)
-        smtp.sendmail(smtp_user, [to], mail.as_bytes(
+        smtp.sendmail(smtp_user, [to], mail.as_string(
             unixfrom=True, policy=email.policy.SMTPUTF8))
     smtp.quit()
 
@@ -300,7 +300,7 @@ Feel free to reply to this email if you have any questions.""".format(
     smtp.starttls()
     if smtp_user and smtp_password:
         smtp.login(smtp_user, smtp_password)
-    smtp.sendmail(smtp_user, [sender[1]], reply.as_bytes(
+    smtp.sendmail(smtp_user, [sender[1]], reply.as_string(
         unixfrom=True, policy=email.policy.SMTPUTF8))
     smtp.quit()
     db.session.commit()
@@ -352,7 +352,7 @@ Feel free to reply to this email if you have any questions.""".format(
     smtp.starttls()
     if smtp_user and smtp_password:
         smtp.login(smtp_user, smtp_password)
-    smtp.sendmail(smtp_user, [sender[1]], reply.as_bytes(
+    smtp.sendmail(smtp_user, [sender[1]], reply.as_string(
         unixfrom=True, policy=email.policy.SMTPUTF8))
     smtp.quit()
     db.session.commit()
@@ -395,7 +395,7 @@ def _configure_mirror(ml, mirror, mail):
     smtp.starttls()
     if smtp_user and smtp_password:
         smtp.login(smtp_user, smtp_password)
-    smtp.sendmail(smtp_user, [sender[1]], reply.as_bytes(
+    smtp.sendmail(smtp_user, [sender[1]], reply.as_string(
         unixfrom=True, policy=email.policy.SMTPUTF8))
     smtp.quit()
     db.session.commit()
