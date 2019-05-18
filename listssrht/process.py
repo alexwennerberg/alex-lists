6c399e7ac16a63df628765e8678890db3aee0c43
Drew DeVault <sir@cmpwn.com>
2019-05-18 10:50:22 -0400

    Use as_bytes instead of as_string
    Turns out we need both SMTPUTF8 and this to get it to do the right
    goddamn thing

--- listssrht/process.py
+++ listssrht/process.py
@@ -70,7 +70,7 @@ def _forward(dest, mail):
             print(to + " is already copied, skipping")
             continue
         print("Forwarding message to " + to)
-        smtp.sendmail(smtp_user, [to], mail.as_string(
+        smtp.sendmail(smtp_user, [to], mail.as_bytes(
             unixfrom=True, policy=email.policy.SMTPUTF8))
     smtp.quit()
 
@@ -146,7 +146,7 @@ def _archive(dest, envelope):
     mail.headers = {
         key: value for key, value in envelope.items()
     }
-    mail.envelope = envelope.as_string(unixfrom=True, maxheaderlen=998)
+    mail.envelope = envelope.as_bytes(unixfrom=True, maxheaderlen=998)
     mail.list_id = dest.id
     for part in envelope.walk():
         if part.is_multipart():
@@ -300,7 +300,7 @@ Feel free to reply to this email if you have any questions.""".format(
     smtp.starttls()
     if smtp_user and smtp_password:
         smtp.login(smtp_user, smtp_password)
-    smtp.sendmail(smtp_user, [sender[1]], reply.as_string(
+    smtp.sendmail(smtp_user, [sender[1]], reply.as_bytes(
         unixfrom=True, policy=email.policy.SMTPUTF8))
     smtp.quit()
     db.session.commit()
@@ -352,7 +352,7 @@ Feel free to reply to this email if you have any questions.""".format(
     smtp.starttls()
     if smtp_user and smtp_password:
         smtp.login(smtp_user, smtp_password)
-    smtp.sendmail(smtp_user, [sender[1]], reply.as_string(
+    smtp.sendmail(smtp_user, [sender[1]], reply.as_bytes(
         unixfrom=True, policy=email.policy.SMTPUTF8))
     smtp.quit()
     db.session.commit()
@@ -395,7 +395,7 @@ def _configure_mirror(ml, mirror, mail):
     smtp.starttls()
     if smtp_user and smtp_password:
         smtp.login(smtp_user, smtp_password)
-    smtp.sendmail(smtp_user, [sender[1]], reply.as_string(
+    smtp.sendmail(smtp_user, [sender[1]], reply.as_bytes(
         unixfrom=True, policy=email.policy.SMTPUTF8))
     smtp.quit()
     db.session.commit()
