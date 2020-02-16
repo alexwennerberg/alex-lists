47817dbea2e9fe7898db2582061c0de3c5751e26
Drew DeVault <sir@cmpwn.com>
2020-02-16 10:44:31 -0500

    process: correct error in emails w/o In-Reply-To

--- listssrht/process.py
+++ listssrht/process.py
@@ -175,7 +175,9 @@ def _archive(dest, envelope):
     mail.patch()
 
     mail.is_request_pull = False # TODO: Detect git request-pull
-    reply_to = envelope["In-Reply-To"].strip()
+    reply_to = envelope["In-Reply-To"]
+    if reply_to:
+        reply_to = reply_to.strip()
 
     db.session.add(mail)
     db.session.flush() # obtain an ID for this email
