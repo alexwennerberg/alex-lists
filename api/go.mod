978ea5bf903dcb5e378d3c4de645d5801ecf977b
Drew DeVault <sir@cmpwn.com>
2021-05-17 11:10:51 -0400

    API: Initial scaffolding

---
+++ api/go.mod
@@ -0,0 +1,9 @@
+module git.sr.ht/~sircmpwn/lists.sr.ht/api
+
+go 1.16
+
+require (
+	git.sr.ht/~sircmpwn/core-go v0.0.0-20210517121848-7ccaeb924e86 // indirect
+	github.com/99designs/gqlgen v0.13.0
+	github.com/vektah/gqlparser/v2 v2.1.0
+)
---
+++ api/graph/schema.resolvers.go
@@ -0,0 +1,38 @@
+package graph
+
+// This file will be automatically regenerated based on the schema, any resolver implementations
+// will be copied through when generating and any unknown code will be moved to the end.
+
+import (
+	"context"
+	"fmt"
+
+	"git.sr.ht/~sircmpwn/lists.sr.ht/api/graph/api"
+	"git.sr.ht/~sircmpwn/lists.sr.ht/api/graph/model"
+)
+
+func (r *queryResolver) Version(ctx context.Context) (*model.Version, error) {
+	return &model.Version{
+		Major:           0,
+		Minor:           0,
+		Patch:           0,
+		DeprecationDate: nil,
+	}, nil
+}
+
+func (r *queryResolver) Me(ctx context.Context) (*model.User, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *queryResolver) UserByID(ctx context.Context, id int) (*model.User, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *queryResolver) UserByName(ctx context.Context, username string) (*model.User, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+// Query returns api.QueryResolver implementation.
+func (r *Resolver) Query() api.QueryResolver { return &queryResolver{r} }
+
+type queryResolver struct{ *Resolver }
