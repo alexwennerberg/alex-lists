902ec2746da2043b70107451d8a1792c1a221521
Conrad Hoffmann <ch@bitfehler.net>
2024-11-08 15:30:39 +0100

    Reduce setup.py to a stub
    All configuration is now in pyproject.toml, all tooling set up to use
    it. The stub remains (for now) to execute the call to `make`, as such
    calls are not supported by pyproject.toml.

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from distutils.core import setup
+from setuptools import setup
 import subprocess
 import os
 import sys
@@ -13,43 +13,4 @@ subp = subprocess.run([make, "SRHT_PATH=" + srht_path])
 if subp.returncode != 0:
     sys.exit(subp.returncode)
 
-ver = os.environ.get("PKGVER") or subprocess.run(['git', 'describe', '--tags'],
-      stdout=subprocess.PIPE).stdout.decode().strip()
-
-setup(
-  name = 'listssrht',
-  packages = [
-      'listssrht',
-      'listssrht.types',
-      'listssrht.blueprints',
-      'listssrht.blueprints.api',
-      'listssrht.alembic',
-      'listssrht.alembic.versions'
-  ],
-  version = ver,
-  description = 'lists.sr.ht website',
-  author = 'Drew DeVault',
-  author_email = 'sir@cmpwn.com',
-  url = 'https://git.sr.ht/~sircmpwn/lists.sr.ht',
-  install_requires = [
-      'srht',
-      'aiosmtpd',
-      'asyncpg',
-      'pygit2',
-  ],
-  license = 'AGPL-3.0',
-  package_data={
-      'listssrht': [
-          'templates/*.html',
-          'static/icons/*',
-          'static/*',
-          'schema.graphqls',
-          'default_query.graphql',
-      ]
-  },
-  scripts = [
-      'listssrht-initdb',
-      'listssrht-lmtp',
-      'listssrht-migrate',
-  ],
-)
+setup()
