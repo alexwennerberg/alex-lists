alexwennerberg/alex-lists

commit 9df239b210e6f152206d4f0e26ce31a5296495b6

api: add archiveMessage internal endpoint
Add a new endpoint that will be used by the ingress daemon to store the
emails to the database.

The imported email is not checked for validity. The responsibility is
deferred to the ingress daemon.

Mark the endpoint as @internal to avoid any exposure to the public.

Signed-off-by: Robin Jarry <robin@jarry.cc>

api/lists/archive.go
@@ -11,6 +11,7 @@ import (
 	"log"
 	"strings"
 
+	apiErr "git.sr.ht/~sircmpwn/lists.sr.ht/api/errors"
 	"git.sr.ht/~sircmpwn/lists.sr.ht/api/graph/model"
 	"github.com/emersion/go-mbox"
 	"github.com/emersion/go-message/mail"
@@ -56,6 +57,9 @@ func (ar *Archiver) ImportSpool(spool io.Reader) error {
 		}
 
 		if err = ar.ArchiveMessage(msg); err != nil {
+			if errors.Is(err, apiErr.ErrDuplicateEmail) {
+				continue
+			}
 			// TODO: Collect errors and email them to the user
 			log.Printf("Error importing message: %v", err)
 		}
@@ -141,7 +145,8 @@ func (ar *Archiver) ArchiveMessage(r io.Reader) error {
 	}
 	if exists {
 		// Skip this message
-		return fmt.Errorf("Skipping duplicate message %q", messageID)
+		log.Printf("Skipping duplicate message %q", messageID)
+		return apiErr.ErrDuplicateEmail
 	}
 
 	var emailID int32
@@ -249,14 +254,17 @@ func (ar *Archiver) ArchiveMessage(r io.Reader) error {
 	}
 
 	status := string(model.PatchsetStatusProposed)
-	const statusHeader = "X-Sourcehut-Patchset-Final"
-	if mr.Header.Has(statusHeader) {
-		s := mr.Header.Get(statusHeader)
-		if model.PatchsetStatus(strings.ToUpper(s)).IsValid() {
-			status = s
+	if ar.isImport {
+		// Only allow forcing patchset status when importing from mbox
+		const statusHeader = "X-Sourcehut-Patchset-Final"
+		if mr.Header.Has(statusHeader) {
+			s := mr.Header.Get(statusHeader)
+			if model.PatchsetStatus(strings.ToUpper(s)).IsValid() {
+				status = s
+			}
 		}
+		status = strings.ToLower(status)
 	}
-	status = strings.ToLower(status)
 
 	if err := ar.importPatch(emailID, threadID, subject, status, isPatch); err != nil {
 		return err

api/errors/errors.go
@@ -24,4 +24,7 @@ var (
 	ErrInvalidToken = newGqlError(
 		"Invalid subscription token.",
 		"ERR_INVALID_TOKEN")
+	ErrDuplicateEmail = newGqlError(
+		"Message already archived in this list.",
+		"ERR_DUPLICATE_EMAIL")
 )

api/graph/schema.graphqls
@@ -794,6 +794,12 @@ type Mutation {
   deleteUser: Int! @internal
 
   """
+  Archives an email (RFC 2045 MIME message) into a mailing list.
+  Internal use only.
+  """
+  archiveMessage(listID: Int!, message: Upload!): Boolean! @internal
+
+  """
   Request a subscription to a mailing list for a given email address. If the
   email is linked to a registered account, the subscription will be associated
   to the account. Otherwise, it will be associated to the mailbox. If a pending

api/graph/schema.resolvers.go
@@ -1411,6 +1411,23 @@ func (r *mutationResolver) DeleteUser(ctx context.Context) (int, error) {
 	return user.UserID, nil
 }
 
+// ArchiveMessage is the resolver for the archiveMessage field.
+func (r *mutationResolver) ArchiveMessage(ctx context.Context, listID int, message graphql.Upload) (bool, error) {
+	if message.Size > 32*1024*1024 {
+		// This is much higher than the default max in the ingress worker
+		return false, errors.New("Messages must not exceed 30 MiB in size")
+	}
+
+	if err := database.WithTx(ctx, nil, func(tx *sql.Tx) error {
+		return lists.NewArchiver(ctx, tx, listID).
+			ArchiveMessage(message.File)
+	}); err != nil {
+		return false, err
+	}
+
+	return true, nil
+}
+
 // RequestSubscription is the resolver for the requestSubscription field.
 func (r *mutationResolver) RequestSubscription(ctx context.Context, listID int, email string) (string, error) {
 	var confirmToken string

commit b481728513693b6bda5065ade6c5bc9dfa72b96d

api: fix initial patchset status when archiving a single message
When archiving a single message, the patchset status is inserted in the
database with the wrong casing "PROPOSED" (instead of the required
lowercase "proposed").

Make sure the status is always lowercase in the database. For good
measure, also check that the imported status is a valid enum value.

Fixes: 9df239b210e6 ("api: add archiveMessage internal endpoint")
Signed-off-by: Robin Jarry <robin@jarry.cc>

api/lists/archive.go
@@ -264,7 +264,6 @@ func (ar *Archiver) ArchiveMessage(r io.Reader) error {
 				status = s
 			}
 		}
-		status = strings.ToLower(status)
 	}
 
 	if err := ar.importPatch(emailID, threadID, subject, status, isPatch); err != nil {

api/lists/patchset.go
@@ -95,6 +95,12 @@ func (ar *Archiver) importPatch(emailID, threadID int32, subject, status string,
 		return nil
 	}
 
+	// check status validity
+	if !model.PatchsetStatus(strings.ToUpper(status)).IsValid() {
+		return fmt.Errorf("invalid status %q", status)
+	}
+	status = strings.ToLower(status)
+
 	// Consider cover letters (index = 0) as valid patches. This makes it much
 	// easier to look up the cover letter later on by querying patch_index = 0.
 	// This also handles the case where the cover letter is received last.