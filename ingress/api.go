alexwennerberg/alex-lists

commit d162fddce831f0763dd2d96cf05f49633f4b70d0

Reimplement the ingress daemon using go
Rename the binary from listssrht-lmtp to ingress/ingress as requested by
Drew since it can be used either with smtp or lmtp. sr.ht-pkgbuilds will
need to be updated accordingly.

No more celery stuff. dowork.Queue is used to avoid blocking the SMTP
ingress (see config.example.ini).

The SMTP server is only processing the strict minimum before passing the
message to one of the ingress workers which takes care of validating the
message, archiving it, looking up the destinations and finally passing
it to another worker via the default core-go email egress queue.

The ingress and egress queue sizes can be configured with:

  [mail]
  egress-queue-size = 512

And:

  [lists.sr.ht::worker]
  ingress-queue-size = 512

The number of workers spawned for both ingress and egress queues can be
configured with:

  [lists.sr.ht::worker]
  queue-workers = 1

If any of the ingress or egress tasks fail, retry executing them 10
times with exponential backoff.

The database is not accessed anymore. The graphql api is used instead.

Most of the work is done in the api daemon behind the archiveMessage
endpoint. The ingress daemon only takes care of the email validation and
dispatch.

Signed-off-by: Robin Jarry <robin@jarry.cc>

ingress/api.go
@@ -1,0 +1,285 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+// Copyright (c) 2024 Robin Jarry
+
+package main
+
+import (
+	"bytes"
+	"context"
+	"strings"
+
+	"git.sr.ht/~sircmpwn/core-go/client"
+	"git.sr.ht/~sircmpwn/core-go/config"
+	"github.com/99designs/gqlgen/graphql"
+	"github.com/emersion/go-message"
+	"github.com/emersion/go-message/mail"
+)
+
+const (
+	LISTS_SERVICE = "lists.sr.ht"
+)
+
+func GqlQueryUser(
+	username string, query string, variables map[string]any, result any,
+) error {
+	ctx := config.Context(context.TODO(), SrhtConfig, LISTS_SERVICE)
+	return client.Do(
+		ctx, username, LISTS_SERVICE,
+		client.GraphQLQuery{Query: query, Variables: variables},
+		result,
+	)
+}
+
+func parseListAddr(addr string) (owner, name string, cmd Command, err error) {
+	cmd = CMD_POST
+
+	// Note: we assume postfix took care of the domain
+	listName, _, _ := strings.Cut(addr, "@")
+	if i := strings.LastIndex(listName, "+"); i > 0 {
+		cmd = Command(listName[i+1:])
+		listName = listName[:i]
+	}
+	if redirect, ok := Config.Redirects[listName]; ok {
+		listName = redirect
+	}
+
+	// split the list name into owner / listname
+	if strings.HasPrefix(listName, "~") {
+		var found bool
+		owner, name, found = strings.Cut(listName, "/")
+		if !found {
+			err = &UnknownListError{addr}
+			return
+		}
+		owner = strings.TrimPrefix(owner, "~")
+	} else {
+		// some mail providers do not allow "~" and "/" in addresses
+		tokens := strings.Split(listName, ".")
+		if len(tokens) < 3 || tokens[0] != "u" {
+			err = &UnknownListError{addr}
+			return
+		}
+		owner = tokens[1]
+		name = strings.Join(tokens[2:], ".")
+	}
+
+	return
+}
+
+func LookupEmailDetails(msg *message.Entity, listAddr string) (*Sender, *MailingList, error) {
+	fromAddr := msg.Header.Get("From")
+	msgID := msg.Header.Get("Message-ID")
+
+	from, err := mail.ParseAddress(fromAddr)
+	if err != nil {
+		return nil, nil, err
+	}
+	owner, list, cmd, err := parseListAddr(listAddr)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var result struct {
+		User *struct {
+			List *struct {
+				ID         int      `json:"id"`
+				PermitMime []string `json:"permitMime"`
+				RejectMime []string `json:"rejectMime"`
+				ACL        Access   `json:"userACL"`
+				ParentMsg  *struct {
+					ID int `json:"id"`
+				} `json:"message"`
+			} `json:"list"`
+		} `json:"user"`
+	}
+
+	err = GqlQueryUser(
+		owner,
+		`query($owner: String!, $list: String!, $from: String!, $msg: String!) {
+			user(username: $owner) {
+				list(name: $list) {
+					id
+					permitMime
+					rejectMime
+					userACL(email: $from) {
+						browse
+						reply
+						post
+						moderate
+					}
+					message(messageID: $msg) {
+						id
+					}
+				}
+			}
+		}`,
+		map[string]any{
+			"owner": owner,
+			"list":  list,
+			"from":  from.Address,
+			"msg":   msgID,
+		},
+		&result,
+	)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	if result.User == nil || result.User.List == nil {
+		return nil, nil, &UnknownListError{listAddr}
+	}
+
+	sender := &Sender{
+		Name:  from.Name,
+		Email: from.Address,
+		ACL:   result.User.List.ACL,
+	}
+
+	mailingList := &MailingList{
+		Owner:           owner,
+		Name:            list,
+		Command:         cmd,
+		ID:              result.User.List.ID,
+		PermitMimetypes: result.User.List.PermitMime,
+		RejectMimetypes: result.User.List.RejectMime,
+		IsReply:         result.User.List.ParentMsg != nil,
+	}
+
+	switch cmd {
+	case CMD_SUBSCRIBE, CMD_UNSUBSCRIBE, CMD_CONFIRM_SUB, CMD_CONFIRM_UNSUB, CMD_POST:
+		return sender, mailingList, nil
+	default:
+		return nil, nil, &UnknownCommandError{mailingList}
+	}
+}
+
+func RequestSubscription(sender *Sender, list *MailingList) (string, error) {
+	var result struct {
+		Token string `json:"requestSubscription"`
+	}
+	err := GqlQueryUser(
+		list.Owner,
+		`mutation($list: Int!, $email: String!) {
+			requestSubscription(listID: $list, email: $email)
+		}`,
+		map[string]any{"list": list.ID, "email": sender.Email},
+		&result,
+	)
+	if err != nil {
+		return "", err
+	}
+	return result.Token, nil
+}
+
+func ConfirmSubscription(sender *Sender, list *MailingList, token string) error {
+	return GqlQueryUser(
+		list.Owner,
+		`mutation($token: ConfirmationToken!, $email: String!) {
+			confirmSubscription(token: $token, email: $email) {
+				id
+			}
+		}`,
+		map[string]any{"token": token, "email": sender.Email},
+		nil,
+	)
+}
+
+func RequestUnsubscription(sender *Sender, list *MailingList) (string, error) {
+	var result struct {
+		Token string `json:"requestUnsubscription"`
+	}
+	err := GqlQueryUser(
+		list.Owner,
+		`mutation($list: Int!, $email: String!) {
+			requestUnsubscription(listID: $list, email: $email)
+		}`,
+		map[string]any{"list": list.ID, "email": sender.Email},
+		&result,
+	)
+	if err != nil {
+		return "", err
+	}
+	return result.Token, nil
+}
+
+func ConfirmUnsubscription(sender *Sender, list *MailingList, token string) error {
+	return GqlQueryUser(
+		list.Owner,
+		`mutation($token: ConfirmationToken!, $email: String!) {
+			confirmUnsubscription(token: $token, email: $email) {
+				id
+			}
+		}`,
+		map[string]any{"token": token, "email": sender.Email},
+		nil,
+	)
+}
+
+func ArchiveMessage(msg *message.Entity, list *MailingList) error {
+	var buf bytes.Buffer
+	if err := msg.WriteTo(&buf); err != nil {
+		return err
+	}
+
+	ctx := config.Context(context.TODO(), SrhtConfig, LISTS_SERVICE)
+	return client.Do(
+		ctx, list.Owner, LISTS_SERVICE,
+		client.GraphQLQuery{
+			Query: `mutation($list: Int!, $msg: Upload!) {
+				archiveMessage(listID: $list, message: $msg)
+			}`,
+			Variables: map[string]any{"list": list.ID},
+			Uploads: map[string]graphql.Upload{
+				"msg": {
+					Filename:    "archive",
+					File:        bytes.NewReader(buf.Bytes()),
+					ContentType: "message/rfc822",
+				},
+			},
+		},
+		nil,
+	)
+}
+
+func LookupSubscribers(list *MailingList) ([]string, error) {
+	var result struct {
+		User struct {
+			List struct {
+				Subscriptions []struct {
+					Subscriber struct {
+						Email string `json:"email"`
+					} `json:"subscriber"`
+				} `json:"subscriptions"`
+			} `json:"list"`
+		} `json:"user"`
+	}
+	err := GqlQueryUser(
+		list.Owner,
+		`query($owner: String!, $list: String!) {
+			user(username: $owner) {
+				list(name: $list) {
+					subscriptions {
+						subscriber {
+							... on Mailbox {
+								email: address
+							}
+							... on User {
+								email
+							}
+						}
+					}
+				}
+			}
+		}`,
+		map[string]any{"owner": list.Owner, "list": list.Name},
+		&result,
+	)
+	if err != nil {
+		return nil, err
+	}
+	emails := make([]string, 0, len(result.User.List.Subscriptions))
+	for _, s := range result.User.List.Subscriptions {
+		emails = append(emails, s.Subscriber.Email)
+	}
+	return emails, nil
+}

ingress/backend.go
@@ -1,0 +1,214 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+// Copyright (c) 2024 Robin Jarry
+
+package main
+
+import (
+	"bytes"
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"strings"
+
+	"git.sr.ht/~sircmpwn/core-go/config"
+	"git.sr.ht/~sircmpwn/core-go/email"
+	work "git.sr.ht/~sircmpwn/dowork"
+	"github.com/emersion/go-message"
+	"github.com/emersion/go-smtp"
+)
+
+// The Backend implements SMTP server methods.
+type Backend struct {
+	ingress *work.Queue
+	ctx     context.Context
+}
+
+// NewSession is called after client greeting (EHLO, HELO).
+func (b *Backend) NewSession(c *smtp.Conn) (smtp.Session, error) {
+	if addr, ok := c.Conn().RemoteAddr().(*net.TCPAddr); ok {
+		if !config.IsInternalIP(addr.IP) {
+			RejectedCounter.Inc()
+			return nil, fmt.Errorf("peer not in internal-ipnet")
+		}
+	}
+	s := &Session{remote: c.Conn().RemoteAddr(), backend: b}
+	log.Printf("connection accepted: %s", s.remote)
+	return s, nil
+}
+
+// A Session is returned after successful login.
+type Session struct {
+	remote  net.Addr
+	from    string
+	to      []string
+	backend *Backend
+}
+
+// Discard currently processed message.
+func (s *Session) Reset() {
+	s.from = ""
+	s.to = nil
+}
+
+// Free all resources associated with session.
+func (s *Session) Logout() error {
+	return nil
+}
+
+// Authenticate the user using SASL PLAIN.
+func (s *Session) AuthPlain(username, password string) error {
+	return nil
+}
+
+// Set return path for currently processed message.
+func (s *Session) Mail(from string, opts *smtp.MailOptions) error {
+	s.from = from
+	return nil
+}
+
+// Add recipient for currently processed message.
+func (s *Session) Rcpt(to string, opts *smtp.RcptOptions) error {
+	s.to = append(s.to, to)
+	return nil
+}
+
+// Read currently processed message contents.
+//
+// r must be consumed before Data returns.
+func (s *Session) Data(r io.Reader) error {
+	var (
+		email *message.Entity
+		from  string
+		to    []string
+		err   error
+		data  bytes.Buffer
+		n     int64
+	)
+
+	EmailsCounter.Inc()
+
+	n, err = io.CopyN(&data, r, Config.MaxMessageSize)
+	switch {
+	case n == Config.MaxMessageSize:
+		err = fmt.Errorf("Message too big.")
+		// drain whatever is left in the pipe
+		_, _ = io.Copy(io.Discard, r)
+		goto end
+	case errors.Is(err, io.EOF):
+		// message was smaller than max size
+		break
+	case err != nil:
+		goto end
+	}
+
+	email, err = message.Read(&data)
+	if err != nil {
+		goto end
+	}
+	switch strings.ToLower(email.Header.Get("Auto-Submitted")) {
+	case "auto-generated", "auto-replied":
+		// disregard automatic emails like OOO replies
+		log.Printf(
+			"ignoring automatic message from=%s to=%s message_id=%s subject=%s",
+			s.from,
+			strings.Join(s.to, ","),
+			email.Header.Get("Message-Id"),
+			email.Header.Get("Subject"),
+		)
+		DroppedCounter.Inc()
+		goto end
+	}
+
+	log.Printf(
+		" message received from=%s to=%s message_id=%s subject=%s",
+		s.from,
+		strings.Join(s.to, ","),
+		email.Header.Get("Message-Id"),
+		email.Header.Get("Subject"),
+	)
+
+	// Make local copies of the values before to ensure the references will
+	// still be valid when the queued task function is evaluated.
+	from = s.from
+	to = s.to
+
+	// Defer processing to the ingress worker.
+	s.backend.ingress.Enqueue(work.NewTask(func(context.Context) error {
+		return s.backend.ProcessMessage(from, to, email)
+	}).Retries(10))
+
+end:
+	s.to = nil
+	s.from = ""
+	if err != nil {
+		ErrorsCounter.Inc()
+	}
+	return err
+}
+
+func (b *Backend) ProcessMessage(
+	from string, recipients []string, email *message.Entity,
+) error {
+	var (
+		list   *MailingList
+		sender *Sender
+		failed int
+		err    error
+	)
+
+	for _, to := range recipients {
+		sender, list, err = LookupEmailDetails(email, to)
+		if err != nil {
+			goto next
+		}
+		switch list.Command {
+		case CMD_SUBSCRIBE:
+			CommandsCounter.Inc()
+			err = b.Subscribe(sender, email, list)
+		case CMD_UNSUBSCRIBE:
+			CommandsCounter.Inc()
+			err = b.Unsubscribe(sender, email, list)
+		case CMD_CONFIRM_SUB:
+			CommandsCounter.Inc()
+			err = b.ConfirmSubscribe(sender, email, list)
+		case CMD_CONFIRM_UNSUB:
+			CommandsCounter.Inc()
+			err = b.ConfirmUnsubscribe(sender, email, list)
+		default:
+			err = b.Post(sender, email, list)
+		}
+	next:
+		if _, ok := err.(BounceError); ok {
+			b.Bounce(email, from, err)
+		} else if err != nil {
+			ErrorsCounter.Inc()
+			log.Printf("ProcessMessage: %s", err)
+			failed++
+		}
+	}
+	if failed != 0 {
+		return fmt.Errorf("ingress to %d lists failed", failed)
+	}
+	return nil
+}
+
+// Instead of letting postfix send an unfriendly bounce message, for some errors
+// we send our own bounce message which is a little easier to understand.
+func (b *Backend) Bounce(msg *message.Entity, to string, err error) {
+	var body bytes.Buffer
+
+	subject := SubjectFallback(msg, "Your recent email to "+Config.Domain)
+	header := ReplyHeaders(Config.MailerAddr, "Re: "+subject, msg)
+	header.Set("Reply-To", Config.OwnerAddr)
+
+	body.WriteString(strings.TrimSpace(err.Error()))
+
+	log.Printf("bouncing message %s: %T", msg.Header.Get("Message-Id"), err)
+	BounceCounter.Inc()
+
+	reply, _ := message.New(header.Header, &body)
+	email.SendRaw(b.ctx, reply, []string{to})
+}

ingress/post.go
@@ -1,0 +1,189 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+// Copyright (c) 2024 Robin Jarry
+
+package main
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"path/filepath"
+	"strings"
+
+	"git.sr.ht/~sircmpwn/core-go/email"
+	apierr "git.sr.ht/~sircmpwn/lists.sr.ht/api/errors"
+	"github.com/emersion/go-message"
+	"github.com/emersion/go-message/mail"
+)
+
+func (b *Backend) Post(sender *Sender, msg *message.Entity, list *MailingList) error {
+	if !(sender.ACL.Post || (list.IsReply && sender.ACL.Reply)) {
+		return &PostPermError{sender, list}
+	}
+
+	if err := Validate(sender, msg, list); err != nil {
+		return err
+	}
+
+	msgID := msg.Header.Get("Message-ID")
+
+	err := ArchiveMessage(msg, list)
+
+	switch {
+	case err == nil:
+		log.Printf("Archived %s to %s", msgID, list.FullName())
+		return b.ForwardMessage(msg, list)
+
+	case errors.Is(err, apierr.ErrDuplicateEmail):
+		log.Printf("Dropping duplicate message %s on %s", msgID, list.FullName())
+		return nil
+
+	default:
+		return err
+	}
+}
+
+var reservedHeaders = []string{
+	"List-Unsubscribe",
+	"List-Subscribe",
+	"List-Archive",
+	"List-Post",
+	"List-ID",
+	"Sender",
+}
+
+func (b *Backend) ForwardMessage(msg *message.Entity, list *MailingList) error {
+	var recipients []string
+	alreadyCopied := make(map[string]bool)
+
+	// fetch subscriber emails from database
+	subscribers, err := LookupSubscribers(list)
+	if err != nil {
+		return err
+	}
+
+	// eliminate recipients that were already included in the original message
+	header := mail.Header{Header: msg.Header}
+	for _, name := range []string{"From", "To", "Cc"} {
+		addresses, _ := header.AddressList(name)
+		for _, addr := range addresses {
+			alreadyCopied[addr.Address] = true
+		}
+	}
+	for _, email := range subscribers {
+		if !alreadyCopied[email] {
+			recipients = append(recipients, email)
+		}
+	}
+
+	msgID := msg.Header.Get("Message-Id")
+
+	if len(recipients) == 0 {
+		log.Printf("No recipients to forward message %s to.", msgID)
+		return nil
+	}
+
+	// prepare message with appropriate mailing list headers
+	for _, h := range reservedHeaders {
+		msg.Header.Del(h)
+	}
+	msg.Header.Set("List-Unsubscribe",
+		fmt.Sprintf("<mailto:%s?subject=unsubscribe>",
+			list.PlusAddress(CMD_UNSUBSCRIBE)))
+	msg.Header.Set("List-Subscribe",
+		fmt.Sprintf("<mailto:%s?subject=subscribe>",
+			list.PlusAddress(CMD_SUBSCRIBE)))
+	msg.Header.Set("List-Archive",
+		fmt.Sprintf("<%s/%s>", Config.OriginUrl, list.FullName()))
+	msg.Header.Set("Archived-At",
+		fmt.Sprintf("<%s/%s/%s>",
+			Config.OriginUrl, list.FullName(),
+			msgID))
+	msg.Header.Set("List-Post",
+		fmt.Sprintf("<mailto:%s>", list.Address()))
+	msg.Header.Set("List-ID",
+		fmt.Sprintf("%s <%s.%s>",
+			list.FullName(), list.FullName(), Config.Domain))
+	msg.Header.Set("Sender",
+		fmt.Sprintf("%s <%s>", list.FullName(), list.Address()))
+
+	// forward the message to all subscribers
+	log.Printf("Forwarding message %s to %d subscribers",
+		msgID, len(recipients))
+	ForwardsCounter.Inc()
+	email.SendRaw(b.ctx, msg, recipients)
+	return nil
+}
+
+var (
+	requiredHeaders   = []string{"From", "Subject", "Message-Id"}
+	prohibitedHeaders = []string{"Return-Receipt-To", "Disposition-Notification-To"}
+)
+
+func Validate(sender *Sender, msg *message.Entity, list *MailingList) error {
+	for _, h := range requiredHeaders {
+		if !msg.Header.Has(h) {
+			return InvalidHeaderErrorf("The %s header is required.", h)
+		}
+	}
+	for _, h := range prohibitedHeaders {
+		if msg.Header.Has(h) {
+			return InvalidHeaderErrorf("The %s header is prohibited.", h)
+		}
+	}
+	if !msg.Header.Has("To") && !msg.Header.Has("Cc") {
+		return InvalidHeaderErrorf("The To or Cc header is required.")
+	}
+	foundTextPart := false
+	var rejected []string
+
+	err := msg.Walk(func(path []int, part *message.Entity, err error) error {
+		if err != nil {
+			return err
+		}
+		contentType, _, err := part.Header.ContentType()
+		if err != nil {
+			contentType = "text/plain"
+		}
+		if strings.HasPrefix(contentType, "multipart/") {
+			return nil
+		}
+		disp, _, err := part.Header.ContentDisposition()
+		if err != nil {
+			disp = "inline"
+		}
+		if contentType == "text/plain" && disp == "inline" {
+			foundTextPart = true
+		}
+		permit := false
+		for _, mime := range list.PermitMimetypes {
+			if match, _ := filepath.Match(mime, contentType); match {
+				permit = true
+				break
+			}
+		}
+		if !permit {
+			rejected = append(rejected, contentType)
+		}
+		for _, mime := range list.RejectMimetypes {
+			if match, _ := filepath.Match(mime, contentType); match {
+				rejected = append(rejected, contentType)
+			}
+		}
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+	if len(rejected) != 0 {
+		if rejected[0] == "text/html" && !foundTextPart {
+			return &HtmlError{sender}
+		} else {
+			return &ForbidenMimeError{sender, rejected[0]}
+		}
+	}
+	if !foundTextPart {
+		return &NoTextError{sender}
+	}
+	return nil
+}

.gitignore
@@ -17,3 +17,4 @@ build
 api/api
 api/graph/api/generated.go
 *_gen.go
+ingress/ingress

Makefile
@@ -2,11 +2,12 @@ SRHT_PATH?=/usr/lib/python3.12/site-packages/srht
 MODULE=listssrht/
 include ${SRHT_PATH}/Makefile
 
-all: api
+all: build
 
-api:
+build:
 	cd api && go generate ./loaders
 	cd api && go generate ./graph
 	cd api && go build
+	cd ingress && go build
 
-.PHONY: all api
+.PHONY: all build

config.example.ini
@@ -36,6 +36,15 @@ network-key=
 # ill effect, if this better suits your infrastructure.
 redis-host=
 
+#
+# Set of IP subnets from which your *.sr.ht services are allowed to
+# contact each other for internal operations.
+#
+# Comma-separated, CIDR notation.
+#
+# Default: 127.0.0.0/8,192.168.0.0/16,10.0.0.0/8,::1/128,fc00::/7
+#internal-ipnet=127.0.0.0/8,::1/128,192.168.0.0/16,10.0.0.0/8,fc00::/7
+
 [mail]
 #
 # Outgoing SMTP settings
@@ -67,6 +76,12 @@ error-from=
 pgp-privkey=
 pgp-pubkey=
 pgp-key-id=
+
+#
+# Queue size for outgoing emails.
+#
+# Default: 512
+#egress-queue-size=512
 
 [webhooks]
 #
@@ -121,21 +136,66 @@ allow-new-lists=yes
 #api-origin=http://localhost:5106
 
 [lists.sr.ht::worker]
+#
 # Protocol used by the daemon. Either lmtp or smtp. By default lmtp if using
 # unix socket and smtp if using tcp socket.
-protocol=lmtp
+#
+# Default; smtp
+#
+#protocol=smtp
+
 #
 # Path for the lmtp daemon's unix socket. Direct incoming mail to this socket.
 # Alternatively, specify IP:PORT will run the server using tcp.
-sock=/tmp/lists.sr.ht-lmtp.sock
+#
+# Default: :25
+#
+#sock=:25
+
+#
+# Queue size for ingress emails from postfix.
+#
+# Default: 512
+#
+#ingress-queue-size=512
+
+#
+# Number of parallel workers that process queued emails.
+#
+# Default: 1
+#
+#queue-workers=1
+
+#
+# Maximum size of messages accepted by the ingress worker. The value must be
+# specified in bytes.
+#
+# Default: 8388608 (8 MiB)
+#
+#max-message-size=8388608
+
+#
+# HTTP endpoint to use for prometheus/openmetrics.
+#
+# Default: :8006
+#
+#metrics-sock=:8006
+
 #
 # The lmtp daemon will make the unix socket group-read/write for users in this
-# group.
-sock-group=postfix
+# group. This setting is ignored when protocol=smtp.
+#
+# Default: postfix
+#
+#sock-group=postfix
+
 #
 # Link to include in the rejection message where senders can get help
 # correcting their email.
-reject-url=https://man.sr.ht/lists.sr.ht/etiquette.md
+#
+# Default: https://useplaintext.email
+#
+#reject-url=https://useplaintext.email
 
 [lists.sr.ht::api]
 #

go.mod
@@ -6,16 +6,19 @@ toolchain go1.23.3
 
 require (
 	git.sr.ht/~emersion/go-emailthreads v0.0.0-20230220165133-75c43015b6c2
-	git.sr.ht/~sircmpwn/core-go v0.0.0-20241216134146-173152054121
+	git.sr.ht/~sircmpwn/core-go v0.0.0-20241218153042-deb699c9d01a
 	git.sr.ht/~sircmpwn/dowork v0.0.0-20241216125407-2b00aa42322c
 	github.com/99designs/gqlgen v0.17.45
 	github.com/Masterminds/squirrel v1.5.4
 	github.com/bluekeyes/go-gitdiff v0.7.2
 	github.com/emersion/go-mbox v1.0.3
 	github.com/emersion/go-message v0.18.0
+	github.com/emersion/go-smtp v0.21.3
 	github.com/go-chi/chi/v5 v5.0.12
 	github.com/google/uuid v1.6.0
 	github.com/lib/pq v1.10.9
+	github.com/prometheus/client_golang v1.19.0
+	github.com/vaughan0/go-ini v0.0.0-20130923145212-a98ad7ee00ec
 	github.com/vektah/dataloaden v0.3.0
 	github.com/vektah/gqlparser/v2 v2.5.11
 )
@@ -32,7 +35,6 @@ require (
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/emersion/go-pgpmail v0.2.1 // indirect
 	github.com/emersion/go-sasl v0.0.0-20231106173351-e73c9f7bad43 // indirect
-	github.com/emersion/go-smtp v0.21.3 // indirect
 	github.com/emersion/go-textwrapper v0.0.0-20200911093747-65d896831594 // indirect
 	github.com/fernet/fernet-go v0.0.0-20240119011108-303da6aec611 // indirect
 	github.com/go-redis/redis/v8 v8.11.5 // indirect
@@ -43,14 +45,12 @@ require (
 	github.com/lann/ps v0.0.0-20150810152359-62de8c46ede0 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	github.com/prometheus/client_golang v1.19.0 // indirect
 	github.com/prometheus/client_model v0.6.0 // indirect
 	github.com/prometheus/common v0.50.0 // indirect
 	github.com/prometheus/procfs v0.13.0 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/sosodev/duration v1.2.0 // indirect
 	github.com/urfave/cli/v2 v2.27.1 // indirect
-	github.com/vaughan0/go-ini v0.0.0-20130923145212-a98ad7ee00ec // indirect
 	github.com/xrash/smetrics v0.0.0-20240312152122-5f08fbb34913 // indirect
 	golang.org/x/crypto v0.21.0 // indirect
 	golang.org/x/mod v0.16.0 // indirect

go.sum
@@ -1,7 +1,7 @@
 git.sr.ht/~emersion/go-emailthreads v0.0.0-20230220165133-75c43015b6c2 h1:5CkkRDW3WrCkoitA4euYbIOVyCAoCh2MnBtGt/neTQc=
 git.sr.ht/~emersion/go-emailthreads v0.0.0-20230220165133-75c43015b6c2/go.mod h1:CQUF7XpyupxjwxlNX3PHRCYL+N2COXTRRRS4MgM49R4=
-git.sr.ht/~sircmpwn/core-go v0.0.0-20241216134146-173152054121 h1:B0DmSkXDuPGBGKmh/KfTOuAx+JydGYT0QIGyQ8dq5Sw=
-git.sr.ht/~sircmpwn/core-go v0.0.0-20241216134146-173152054121/go.mod h1:PiQ/74uq73oRAkP3Wkqm9GCCvN4qNolkbq9xGZK6KN0=
+git.sr.ht/~sircmpwn/core-go v0.0.0-20241218153042-deb699c9d01a h1:mcXDf5QhYUJ2XuQ+i56sK4597YyyO5j0JVXJs7FU4WM=
+git.sr.ht/~sircmpwn/core-go v0.0.0-20241218153042-deb699c9d01a/go.mod h1:PiQ/74uq73oRAkP3Wkqm9GCCvN4qNolkbq9xGZK6KN0=
 git.sr.ht/~sircmpwn/dowork v0.0.0-20241216125407-2b00aa42322c h1:v2opuaN0C5ZpuCifRNR9ZQ8V9IG+Ja80otK1MFj5RnI=
 git.sr.ht/~sircmpwn/dowork v0.0.0-20241216125407-2b00aa42322c/go.mod h1:8neHEO3503w/rNtttnR0JFpQgM/GFhaafVwvkPsFIDw=
 git.sr.ht/~sircmpwn/getopt v0.0.0-20191230200459-23622cc906b3/go.mod h1:wMEGFFFNuPos7vHmWXfszqImLppbc0wEhh6JBfJIUgw=

commit 82638fc6a9b7eae0b91922f9d9f073416cb51bb0

ingress: fix message archival and forwarding
Once created, a message.Entity can only be consumed once. During the
message validation, we walk through all parts to check if there is
a text/plain part and no forbidden MIME types. This effectively consumes
the reader and causes subsequent msg.WriteTo() to only write the message
headers without any body content.

This is silently accepted by all parsers because an empty email with
a single inline text/plain part is valid. However, when dealing with
multipart messages, we get errors from the message parser:

   Error reading message part: multipart: NextPart: EOF

Avoid reusing message.Entity objects. Instead pass down a bytes array to
ProcessMessage() and directly archive it.

Fixes: d162fddce831 ("Reimplement the ingress daemon using go")
Signed-off-by: Robin Jarry <robin@jarry.cc>

ingress/api.go
@@ -215,12 +215,7 @@ func ConfirmUnsubscription(sender *Sender, list *MailingList, token string) erro
 	)
 }
 
-func ArchiveMessage(msg *message.Entity, list *MailingList) error {
-	var buf bytes.Buffer
-	if err := msg.WriteTo(&buf); err != nil {
-		return err
-	}
-
+func ArchiveMessage(data []byte, list *MailingList) error {
 	ctx := config.Context(context.TODO(), SrhtConfig, LISTS_SERVICE)
 	return client.Do(
 		ctx, list.Owner, LISTS_SERVICE,
@@ -232,7 +227,7 @@ func ArchiveMessage(msg *message.Entity, list *MailingList) error {
 			Uploads: map[string]graphql.Upload{
 				"msg": {
 					Filename:    "archive",
-					File:        bytes.NewReader(buf.Bytes()),
+					File:        bytes.NewReader(data),
 					ContentType: "message/rfc822",
 				},
 			},

ingress/backend.go
@@ -84,13 +84,14 @@ func (s *Session) Data(r io.Reader) error {
 		from  string
 		to    []string
 		err   error
-		data  bytes.Buffer
+		buf   bytes.Buffer
+		data  []byte
 		n     int64
 	)
 
 	EmailsCounter.Inc()
 
-	n, err = io.CopyN(&data, r, Config.MaxMessageSize)
+	n, err = io.CopyN(&buf, r, Config.MaxMessageSize)
 	switch {
 	case n == Config.MaxMessageSize:
 		err = fmt.Errorf("Message too big.")
@@ -104,10 +105,13 @@ func (s *Session) Data(r io.Reader) error {
 		goto end
 	}
 
-	email, err = message.Read(&data)
-	if err != nil {
+	data = buf.Bytes()
+
+	email, err = message.Read(bytes.NewReader(data))
+	if err != nil && message.IsUnknownCharset(err) {
 		goto end
 	}
+
 	switch strings.ToLower(email.Header.Get("Auto-Submitted")) {
 	case "auto-generated", "auto-replied":
 		// disregard automatic emails like OOO replies
@@ -123,7 +127,7 @@ func (s *Session) Data(r io.Reader) error {
 	}
 
 	log.Printf(
-		" message received from=%s to=%s message_id=%s subject=%s",
+		"message received from=%s to=%s message_id=%s subject=%s",
 		s.from,
 		strings.Join(s.to, ","),
 		email.Header.Get("Message-Id"),
@@ -137,7 +141,7 @@ func (s *Session) Data(r io.Reader) error {
 
 	// Defer processing to the ingress worker.
 	s.backend.ingress.Enqueue(work.NewTask(func(context.Context) error {
-		return s.backend.ProcessMessage(from, to, email)
+		return s.backend.ProcessMessage(from, to, data)
 	}).Retries(10))
 
 end:
@@ -150,14 +154,18 @@ end:
 }
 
 func (b *Backend) ProcessMessage(
-	from string, recipients []string, email *message.Entity,
+	from string, recipients []string, data []byte,
 ) error {
 	var (
+		email  *message.Entity
 		list   *MailingList
 		sender *Sender
 		failed int
 		err    error
 	)
+
+	// cannot fail, we already parsed it once in Session.Data()
+	email, _ = message.Read(bytes.NewReader(data))
 
 	for _, to := range recipients {
 		sender, list, err = LookupEmailDetails(email, to)
@@ -178,7 +186,7 @@ func (b *Backend) ProcessMessage(
 			CommandsCounter.Inc()
 			err = b.ConfirmUnsubscribe(sender, email, list)
 		default:
-			err = b.Post(sender, email, list)
+			err = b.Post(sender, data, email, list)
 		}
 	next:
 		if _, ok := err.(BounceError); ok {

ingress/post.go
@@ -4,6 +4,7 @@
 package main
 
 import (
+	"bytes"
 	"errors"
 	"fmt"
 	"log"
@@ -16,7 +17,7 @@ import (
 	"github.com/emersion/go-message/mail"
 )
 
-func (b *Backend) Post(sender *Sender, msg *message.Entity, list *MailingList) error {
+func (b *Backend) Post(sender *Sender, data []byte, msg *message.Entity, list *MailingList) error {
 	if !(sender.ACL.Post || (list.IsReply && sender.ACL.Reply)) {
 		return &PostPermError{sender, list}
 	}
@@ -27,12 +28,13 @@ func (b *Backend) Post(sender *Sender, msg *message.Entity, list *MailingList) e
 
 	msgID := msg.Header.Get("Message-ID")
 
-	err := ArchiveMessage(msg, list)
+	// Validate() consumed the message.Entity body, directly archive raw data
+	err := ArchiveMessage(data, list)
 
 	switch {
 	case err == nil:
 		log.Printf("Archived %s to %s", msgID, list.FullName())
-		return b.ForwardMessage(msg, list)
+		return b.ForwardMessage(data, list)
 
 	case errors.Is(err, apierr.ErrDuplicateEmail):
 		log.Printf("Dropping duplicate message %s on %s", msgID, list.FullName())
@@ -52,7 +54,7 @@ var reservedHeaders = []string{
 	"Sender",
 }
 
-func (b *Backend) ForwardMessage(msg *message.Entity, list *MailingList) error {
+func (b *Backend) ForwardMessage(data []byte, list *MailingList) error {
 	var recipients []string
 	alreadyCopied := make(map[string]bool)
 
@@ -61,6 +63,9 @@ func (b *Backend) ForwardMessage(msg *message.Entity, list *MailingList) error {
 	if err != nil {
 		return err
 	}
+
+	// cannot fail, the message has already been validated
+	msg, _ := message.Read(bytes.NewReader(data))
 
 	// eliminate recipients that were already included in the original message
 	header := mail.Header{Header: msg.Header}