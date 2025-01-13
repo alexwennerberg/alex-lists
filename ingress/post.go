alexwennerberg/alex-lists

commit e907c2b805c261f1d71a3dbeebc5ab554c16da0e

ingress: fix corrupted messages after forwarding
For some unknown reason, formatting an email after parsing it with
go-message causes issues. Random line breaks are inserted into the body
of the message. I haven't had the time to properly diagnose what is the
root cause.

To avoid that problem, skip the parsing/reformatting of messages before
forwarding them to subscribers. Simply prepend mailing list headers to
the original buffer. Move "reserved" list headers to the "prohibited"
headers to be handled during the validation phase.

Fixes: d162fddce831 ("Reimplement the ingress daemon using go")
Signed-off-by: Robin Jarry <robin@jarry.cc>

ingress/post.go
@@ -45,15 +45,6 @@ func (b *Backend) Post(sender *Sender, data []byte, msg *message.Entity, list *M
 	}
 }
 
-var reservedHeaders = []string{
-	"List-Unsubscribe",
-	"List-Subscribe",
-	"List-Archive",
-	"List-Post",
-	"List-ID",
-	"Sender",
-}
-
 func (b *Backend) ForwardMessage(data []byte, list *MailingList) error {
 	var recipients []string
 	alreadyCopied := make(map[string]bool)
@@ -88,40 +79,49 @@ func (b *Backend) ForwardMessage(data []byte, list *MailingList) error {
 		return nil
 	}
 
-	// prepare message with appropriate mailing list headers
-	for _, h := range reservedHeaders {
-		msg.Header.Del(h)
+	// prepend message with appropriate mailing list headers
+	var buf bytes.Buffer
+
+	fmt.Fprintf(&buf, "List-Unsubscribe: <mailto:%s?subject=unsubscribe>\r\n",
+		list.PlusAddress(CMD_UNSUBSCRIBE))
+	fmt.Fprintf(&buf, "List-Subscribe: <mailto:%s?subject=subscribe>\r\n",
+		list.PlusAddress(CMD_SUBSCRIBE))
+	fmt.Fprintf(&buf, "List-Archive: <%s/%s>\r\n",
+		Config.OriginUrl, list.FullName())
+	fmt.Fprintf(&buf, "Archived-At: <%s/%s/%s>\r\n",
+		Config.OriginUrl, list.FullName(), msgID)
+	fmt.Fprintf(&buf, "List-Post: <mailto:%s>\r\n",
+		list.Address())
+	fmt.Fprintf(&buf, "List-ID: %s <%s.%s>\r\n",
+		list.FullName(), list.FullName(), Config.Domain)
+	fmt.Fprintf(&buf, "Sender: %s <%s>\r\n",
+		list.FullName(), list.Address())
+
+	// append received message verbatim without reformatting
+	_, err = buf.Write(data)
+	if err != nil {
+		return fmt.Errorf("buf.Write: %w", err)
 	}
-	msg.Header.Set("List-Unsubscribe",
-		fmt.Sprintf("<mailto:%s?subject=unsubscribe>",
-			list.PlusAddress(CMD_UNSUBSCRIBE)))
-	msg.Header.Set("List-Subscribe",
-		fmt.Sprintf("<mailto:%s?subject=subscribe>",
-			list.PlusAddress(CMD_SUBSCRIBE)))
-	msg.Header.Set("List-Archive",
-		fmt.Sprintf("<%s/%s>", Config.OriginUrl, list.FullName()))
-	msg.Header.Set("Archived-At",
-		fmt.Sprintf("<%s/%s/%s>",
-			Config.OriginUrl, list.FullName(),
-			msgID))
-	msg.Header.Set("List-Post",
-		fmt.Sprintf("<mailto:%s>", list.Address()))
-	msg.Header.Set("List-ID",
-		fmt.Sprintf("%s <%s.%s>",
-			list.FullName(), list.FullName(), Config.Domain))
-	msg.Header.Set("Sender",
-		fmt.Sprintf("%s <%s>", list.FullName(), list.Address()))
 
 	// forward the message to all subscribers
 	log.Printf("Forwarding message %s to %d subscribers",
 		msgID, len(recipients))
 	ForwardsCounter.Inc()
-	return email.EnqueueRaw(b.ctx, msg, recipients)
+	return email.EnqueueRaw(b.ctx, buf.Bytes(), recipients)
 }
 
 var (
 	requiredHeaders   = []string{"From", "Subject", "Message-Id"}
-	prohibitedHeaders = []string{"Return-Receipt-To", "Disposition-Notification-To"}
+	prohibitedHeaders = []string{
+		"Return-Receipt-To",
+		"Disposition-Notification-To",
+		"List-Unsubscribe",
+		"List-Subscribe",
+		"List-Archive",
+		"List-Post",
+		"List-ID",
+		"Sender",
+	}
 )
 
 func Validate(sender *Sender, msg *message.Entity, list *MailingList) error {

go.mod
@@ -6,7 +6,7 @@ toolchain go1.23.3
 
 require (
 	git.sr.ht/~emersion/go-emailthreads v0.0.0-20230220165133-75c43015b6c2
-	git.sr.ht/~sircmpwn/core-go v0.0.0-20241218153042-deb699c9d01a
+	git.sr.ht/~sircmpwn/core-go v0.0.0-20250113094522-db4d67a2a6fb
 	git.sr.ht/~sircmpwn/dowork v0.0.0-20241216125407-2b00aa42322c
 	github.com/99designs/gqlgen v0.17.45
 	github.com/Masterminds/squirrel v1.5.4

go.sum
@@ -1,7 +1,7 @@
 git.sr.ht/~emersion/go-emailthreads v0.0.0-20230220165133-75c43015b6c2 h1:5CkkRDW3WrCkoitA4euYbIOVyCAoCh2MnBtGt/neTQc=
 git.sr.ht/~emersion/go-emailthreads v0.0.0-20230220165133-75c43015b6c2/go.mod h1:CQUF7XpyupxjwxlNX3PHRCYL+N2COXTRRRS4MgM49R4=
-git.sr.ht/~sircmpwn/core-go v0.0.0-20241218153042-deb699c9d01a h1:mcXDf5QhYUJ2XuQ+i56sK4597YyyO5j0JVXJs7FU4WM=
-git.sr.ht/~sircmpwn/core-go v0.0.0-20241218153042-deb699c9d01a/go.mod h1:PiQ/74uq73oRAkP3Wkqm9GCCvN4qNolkbq9xGZK6KN0=
+git.sr.ht/~sircmpwn/core-go v0.0.0-20250113094522-db4d67a2a6fb h1:sHIlZ2QNbC9bXJjYyrEWA3au1Ym/1vPPmtDhOcBI8Fg=
+git.sr.ht/~sircmpwn/core-go v0.0.0-20250113094522-db4d67a2a6fb/go.mod h1:PiQ/74uq73oRAkP3Wkqm9GCCvN4qNolkbq9xGZK6KN0=
 git.sr.ht/~sircmpwn/dowork v0.0.0-20241216125407-2b00aa42322c h1:v2opuaN0C5ZpuCifRNR9ZQ8V9IG+Ja80otK1MFj5RnI=
 git.sr.ht/~sircmpwn/dowork v0.0.0-20241216125407-2b00aa42322c/go.mod h1:8neHEO3503w/rNtttnR0JFpQgM/GFhaafVwvkPsFIDw=
 git.sr.ht/~sircmpwn/getopt v0.0.0-20191230200459-23622cc906b3/go.mod h1:wMEGFFFNuPos7vHmWXfszqImLppbc0wEhh6JBfJIUgw=

commit 4a9d0e19c6e56c22378d966e550cf2f0ca1ce6f0

ingress: be more lenient about prohibited headers
Sender should certainly not be prohibited. It is inserted by many
clients. If any of the List-* headers are duplicate, it is fine.

Escape the message-id from the archived URL for good measure.

Fixes: e907c2b805c2 ("ingress: fix corrupted messages after forwarding")
Signed-off-by: Robin Jarry <robin@jarry.cc>

ingress/post.go
@@ -8,6 +8,7 @@ import (
 	"errors"
 	"fmt"
 	"log"
+	"net/url"
 	"path/filepath"
 	"strings"
 
@@ -89,7 +90,7 @@ func (b *Backend) ForwardMessage(data []byte, list *MailingList) error {
 	fmt.Fprintf(&buf, "List-Archive: <%s/%s>\r\n",
 		Config.OriginUrl, list.FullName())
 	fmt.Fprintf(&buf, "Archived-At: <%s/%s/%s>\r\n",
-		Config.OriginUrl, list.FullName(), msgID)
+		Config.OriginUrl, list.FullName(), url.PathEscape(msgID))
 	fmt.Fprintf(&buf, "List-Post: <mailto:%s>\r\n",
 		list.Address())
 	fmt.Fprintf(&buf, "List-ID: %s <%s.%s>\r\n",
@@ -112,16 +113,7 @@ func (b *Backend) ForwardMessage(data []byte, list *MailingList) error {
 
 var (
 	requiredHeaders   = []string{"From", "Subject", "Message-Id"}
-	prohibitedHeaders = []string{
-		"Return-Receipt-To",
-		"Disposition-Notification-To",
-		"List-Unsubscribe",
-		"List-Subscribe",
-		"List-Archive",
-		"List-Post",
-		"List-ID",
-		"Sender",
-	}
+	prohibitedHeaders = []string{"Return-Receipt-To", "Disposition-Notification-To"}
 )
 
 func Validate(sender *Sender, msg *message.Entity, list *MailingList) error {