alexwennerberg/alex-lists

commit 6cc18bbb96fed04d4ea4af0639b767e9af6b9253

api: trigger webhooks after message archival
When a message is archived explicitly (not imported via a mbox spool),
trigger the EMAIL_RECEIVED and PATCHSET_RECEIVED webhooks.

Signed-off-by: Robin Jarry <robin@jarry.cc>

api/lists/archive.go
@@ -13,6 +13,7 @@ import (
 
 	apiErr "git.sr.ht/~sircmpwn/lists.sr.ht/api/errors"
 	"git.sr.ht/~sircmpwn/lists.sr.ht/api/graph/model"
+	"git.sr.ht/~sircmpwn/lists.sr.ht/api/webhooks"
 	"github.com/emersion/go-mbox"
 	"github.com/emersion/go-message/mail"
 	"github.com/lib/pq"
@@ -239,7 +240,7 @@ func (ar *Archiver) ArchiveMessage(r io.Reader) error {
 		`SELECT id FROM "user" WHERE email = $1`,
 		senders[0].Address,
 	)
-	var senderID int
+	var senderID *int
 	if err := row.Scan(&senderID); err != nil {
 		if err != sql.ErrNoRows {
 			return err
@@ -247,7 +248,7 @@ func (ar *Archiver) ArchiveMessage(r io.Reader) error {
 	} else {
 		if _, err := ar.tx.Exec(
 			`UPDATE email SET sender_id = $1 WHERE id = $2`,
-			senderID, emailID,
+			*senderID, emailID,
 		); err != nil {
 			return err
 		}
@@ -268,6 +269,42 @@ func (ar *Archiver) ArchiveMessage(r io.Reader) error {
 
 	if err := ar.importPatch(emailID, threadID, subject, status, isPatch); err != nil {
 		return err
+	}
+
+	if !ar.isImport {
+		var patchsetID *int
+		row = ar.tx.QueryRow(`
+			SELECT patchset_id FROM email
+			WHERE (id = $1 OR thread_id = $1)
+			AND patchset_id IS NOT NULL;
+		`, threadID)
+		err = row.Scan(&patchsetID)
+		if err != nil && err != sql.ErrNoRows {
+			panic(err)
+		}
+
+		tid := new(int)
+		*tid = int(threadID)
+		var irp *string
+		if inReplyTo.Valid {
+			irp = &inReplyTo.String
+		}
+		webhooks.DeliverListEmailEvent(
+			ar.ctx, ar.listID, model.WebhookEventEmailReceived,
+			&model.Email{
+				ID:            int(emailID),
+				Received:      date,
+				Body:          body,
+				Subject:       subject,
+				MessageID:     messageID,
+				MailingListID: ar.listID,
+				RawHeader:     mr.Header,
+				RawEnvelope:   envelope.Bytes(),
+				ThreadID:      tid,
+				PatchsetID:    patchsetID,
+				InReplyTo:     irp,
+			},
+		)
 	}
 
 	log.Printf("Archived message %q", messageID)

api/lists/patchset.go
@@ -7,7 +7,10 @@ import (
 	"regexp"
 	"strconv"
 	"strings"
+	"time"
 
+	"git.sr.ht/~sircmpwn/lists.sr.ht/api/graph/model"
+	"git.sr.ht/~sircmpwn/lists.sr.ht/api/webhooks"
 	"github.com/bluekeyes/go-gitdiff/gitdiff"
 )
 
@@ -197,6 +200,7 @@ func (ar *Archiver) importPatch(emailID, threadID int32, subject, status string,
 	}
 
 	var patchsetID int
+	var created, updated time.Time
 	row = ar.tx.QueryRow(`
 		INSERT INTO patchset (
 			created, updated,
@@ -206,11 +210,11 @@ func (ar *Archiver) importPatch(emailID, threadID int32, subject, status string,
 			NOW() at time zone 'utc',
 			NOW() at time zone 'utc',
 			$1, $2, $3, $4, $5, $6, $7, $8, $9
-		) RETURNING id`,
+		) RETURNING id, created, updated`,
 		patchsetSubject, patchsetPrefix, patchsetVersion, ar.listID, coverLetterID,
 		messageID, submitter, replyTo, status,
 	)
-	if err := row.Scan(&patchsetID); err != nil {
+	if err := row.Scan(&patchsetID, &created, &updated); err != nil {
 		return err
 	}
 
@@ -221,7 +225,22 @@ func (ar *Archiver) importPatch(emailID, threadID int32, subject, status string,
 		return err
 	}
 
-	// TODO: Legacy webhooks
+	if !ar.isImport {
+		webhooks.DeliverListPatchsetEvent(
+			ar.ctx, ar.listID, model.WebhookEventPatchsetReceived,
+			&model.Patchset{
+				ID:            patchsetID,
+				Created:       created,
+				Updated:       updated,
+				Subject:       subject,
+				Prefix:        &patchsetPrefix,
+				Version:       patchsetVersion,
+				MailingListID: ar.listID,
+				CoverLetterID: coverLetterID,
+				RawStatus:     status,
+			},
+		)
+	}
 
 	// TODO: identify patchset that this supersedes, if appropriate
 

commit 2c787e95e831779727f1bf107ef1a4858e43f1a4

archive: fix webhooks execution
The email_received and patchset_received webhooks are currently
triggered in the middle of the very transaction that is creating the
message/patchset rows in the database.

Since webhooks run in a separate transaction, from their point of view,
the objects are not up to date. Depending on the gql queries, some info
may not be available.

Move the execution of webhooks *after* the archive transaction is
complete and successful.

Fixes: 6cc18bbb96fe ("api: trigger webhooks after message archival")
Signed-off-by: Robin Jarry <robin@jarry.cc>

api/graph/schema.resolvers.go
@@ -1449,10 +1449,18 @@ func (r *mutationResolver) ArchiveMessage(ctx context.Context, listID int, messa
 		return false, errors.New("Messages must not exceed 30 MiB in size")
 	}
 
+	var emailID int
+	var err error
+
 	if err := database.WithTx(ctx, nil, func(tx *sql.Tx) error {
-		return lists.NewArchiver(ctx, tx, listID).
+		emailID, err = lists.NewArchiver(ctx, tx, listID).
 			ArchiveMessage(message.File)
+		return err
 	}); err != nil {
+		return false, err
+	}
+
+	if _, err = r.TriggerListEmailWebhooks(ctx, listID, emailID); err != nil {
 		return false, err
 	}
 

api/lists/archive.go
@@ -14,7 +14,6 @@ import (
 
 	apiErr "git.sr.ht/~sircmpwn/lists.sr.ht/api/errors"
 	"git.sr.ht/~sircmpwn/lists.sr.ht/api/graph/model"
-	"git.sr.ht/~sircmpwn/lists.sr.ht/api/webhooks"
 	"github.com/emersion/go-mbox"
 	"github.com/emersion/go-message"
 	"github.com/emersion/go-message/mail"
@@ -59,7 +58,7 @@ func (ar *Archiver) ImportSpool(spool io.Reader) error {
 			return fmt.Errorf("Error reading mailing list spool: %v", err)
 		}
 
-		if err = ar.ArchiveMessage(msg); err != nil {
+		if _, err = ar.ArchiveMessage(msg); err != nil {
 			if errors.Is(err, apiErr.ErrDuplicateEmail) {
 				continue
 			}
@@ -74,17 +73,17 @@ func (ar *Archiver) ImportSpool(spool io.Reader) error {
 // Import a single email (RFC 2045 MIME message) into a mailing list archive.
 //
 // Does not enforce access controls.
-func (ar *Archiver) ArchiveMessage(r io.Reader) error {
+func (ar *Archiver) ArchiveMessage(r io.Reader) (int, error) {
 	var envelope bytes.Buffer
 
 	mr, err := mail.CreateReader(io.TeeReader(r, &envelope))
 	if err != nil {
-		return err
+		return 0, err
 	}
 	subject, err := mr.Header.Subject()
 	if err != nil {
 		if !message.IsUnknownCharset(err) {
-			return fmt.Errorf("Error reading Subject: %w", err)
+			return 0, fmt.Errorf("Error reading Subject: %w", err)
 		}
 		if subject == "" {
 			// even if the subject is garbage, at least store something in the db
@@ -122,7 +121,7 @@ func (ar *Archiver) ArchiveMessage(r io.Reader) error {
 		if err == io.EOF {
 			break
 		} else if err != nil {
-			return fmt.Errorf("Error reading message part: %w", err)
+			return 0, fmt.Errorf("Error reading message part: %w", err)
 		}
 
 		switch p.Header.(type) {
@@ -142,7 +141,7 @@ func (ar *Archiver) ArchiveMessage(r io.Reader) error {
 
 	headerMap, err := json.Marshal(mr.Header.Map())
 	if err != nil {
-		return err
+		return 0, err
 	}
 
 	var exists bool
@@ -153,12 +152,12 @@ func (ar *Archiver) ArchiveMessage(r io.Reader) error {
 		ar.listID, messageID,
 	)
 	if err := row.Scan(&exists); err != nil {
-		return err
+		return 0, err
 	}
 	if exists {
 		// Skip this message
 		log.Printf("Skipping duplicate message %q", messageID)
-		return apiErr.ErrDuplicateEmail
+		return 0, apiErr.ErrDuplicateEmail
 	}
 
 	var emailID int32
@@ -193,7 +192,7 @@ func (ar *Archiver) ArchiveMessage(r io.Reader) error {
 		nil, nil, nil, nil, nil, nil, nil,
 	)
 	if err := row.Scan(&emailID); err != nil {
-		return err
+		return 0, err
 	}
 
 	// Set parent of this email
@@ -204,35 +203,35 @@ func (ar *Archiver) ArchiveMessage(r io.Reader) error {
 	)
 	if err := row.Scan(&parentID); err != nil {
 		if err != sql.ErrNoRows {
-			return err
+			return 0, err
 		}
 	} else {
 		if _, err := ar.tx.Exec(
 			`UPDATE email SET parent_id = $1 WHERE id = $2`,
 			parentID, emailID,
 		); err != nil {
-			return err
+			return 0, err
 		}
 	}
 
 	threadID, err := ar.computeThreadID(emailID)
 	if err != nil {
-		return err
+		return 0, err
 	}
 	if threadID != emailID {
 		if _, err := ar.tx.Exec(
 			`UPDATE email SET thread_id = $1 WHERE id = $2`,
 			threadID, emailID,
 		); err != nil {
-			return err
+			return 0, err
 		}
 	}
 
 	if err := ar.reparentEmails(threadID, emailID, messageID); err != nil {
-		return err
+		return 0, err
 	}
 	if err := ar.updateThreadReplies(threadID); err != nil {
-		return err
+		return 0, err
 	}
 
 	// TODO: Enumerate CC's and create SQL relationships for them
@@ -240,10 +239,10 @@ func (ar *Archiver) ArchiveMessage(r io.Reader) error {
 	// TODO: Multiple From addresses?
 	senders, err := mr.Header.AddressList("From")
 	if err != nil {
-		return fmt.Errorf("Error reading From: %q %w", mr.Header.Get("From"), err)
+		return 0, fmt.Errorf("Error reading From: %q %w", mr.Header.Get("From"), err)
 	}
 	if len(senders) == 0 {
-		return errors.New("expected at least one From address")
+		return 0, errors.New("expected at least one From address")
 	}
 
 	// Lookup sender by email
@@ -254,14 +253,14 @@ func (ar *Archiver) ArchiveMessage(r io.Reader) error {
 	var senderID *int
 	if err := row.Scan(&senderID); err != nil {
 		if err != sql.ErrNoRows {
-			return err
+			return 0, err
 		}
 	} else {
 		if _, err := ar.tx.Exec(
 			`UPDATE email SET sender_id = $1 WHERE id = $2`,
 			*senderID, emailID,
 		); err != nil {
-			return err
+			return 0, err
 		}
 	}
 
@@ -278,7 +277,7 @@ func (ar *Archiver) ArchiveMessage(r io.Reader) error {
 	}
 
 	if err := ar.importPatch(emailID, threadID, subject, status, isPatch); err != nil {
-		return err
+		return 0, err
 	}
 
 	if !ar.isImport {
@@ -304,34 +303,11 @@ func (ar *Archiver) ArchiveMessage(r io.Reader) error {
 				log.Println("Failed updating patchset status:", err)
 			}
 		}
-
-		tid := new(int)
-		*tid = int(threadID)
-		var irp *string
-		if inReplyTo.Valid {
-			irp = &inReplyTo.String
-		}
-		webhooks.DeliverListEmailEvent(
-			ar.ctx, ar.listID, model.WebhookEventEmailReceived,
-			&model.Email{
-				ID:            int(emailID),
-				Received:      date,
-				Body:          body,
-				Subject:       subject,
-				MessageID:     messageID,
-				MailingListID: ar.listID,
-				RawHeader:     mr.Header,
-				RawEnvelope:   envelope.Bytes(),
-				ThreadID:      tid,
-				PatchsetID:    patchsetID,
-				InReplyTo:     irp,
-			},
-		)
 	}
 
 	log.Printf("Archived message %q", messageID)
 
-	return nil
+	return int(emailID), nil
 }
 
 // Computes the thread ID for the given email

api/lists/patchset.go
@@ -10,7 +10,6 @@ import (
 	"time"
 
 	"git.sr.ht/~sircmpwn/lists.sr.ht/api/graph/model"
-	"git.sr.ht/~sircmpwn/lists.sr.ht/api/webhooks"
 	"github.com/bluekeyes/go-gitdiff/gitdiff"
 )
 
@@ -229,23 +228,6 @@ func (ar *Archiver) importPatch(emailID, threadID int32, subject, status string,
 		patchsetID, threadID,
 	); err != nil {
 		return err
-	}
-
-	if !ar.isImport {
-		webhooks.DeliverListPatchsetEvent(
-			ar.ctx, ar.listID, model.WebhookEventPatchsetReceived,
-			&model.Patchset{
-				ID:            patchsetID,
-				Created:       created,
-				Updated:       updated,
-				Subject:       subject,
-				Prefix:        &patchsetPrefix,
-				Version:       patchsetVersion,
-				MailingListID: ar.listID,
-				CoverLetterID: coverLetterID,
-				RawStatus:     status,
-			},
-		)
 	}
 
 	// TODO: identify patchset that this supersedes, if appropriate