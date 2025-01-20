4803578bfc40986c0e04f44d1a8d306e8010a2d3
Conrad Hoffmann <ch@bitfehler.net>
2025-01-20 10:49:48 +0100

    OAuthService: Remove obsolete method
    It's never called and the super method it calls does not exist either.

--- listssrht/oauth.py
+++ listssrht/oauth.py
@@ -17,26 +17,6 @@ class ListsOAuthService(AbstractOAuthService):
             DelegatedScope("subs", "subscriptions", True),
         ], user_class=User, token_class=OAuthToken)
 
-    def lookup_or_register(self, token, token_expires, scopes):
-        user = super().lookup_or_register(token, token_expires, scopes)
-        if not user.id:
-            db.session.flush()
-            # Rewire existing subscriptions
-            for sub in Subscription.query.filter(
-                    Subscription.email == user.email).all():
-                sub.email = None
-                sub.user_id = user.id
-            # Rewire existing emails
-            for email in (Email.query
-                    .filter(Email.sender_id is None)
-                    .filter(sa.cast(Email.headers["From"], sa.String)
-                        .ilike("%" + user.email + "%"))):
-                name, addr = parseaddr(email.headers["From"])
-                if addr == user.email:
-                    email.sender_id = user.id
-        db.session.commit()
-        return user
-
     def profile_update_hook(self, user, payload):
         if user.email != payload["email"]:
             # Adopt emails sent with this address
