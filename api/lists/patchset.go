alexwennerberg/alex-lists

commit cde342868e5def4c9686dfe0cb3b2dcbf12a5696

api: Implement mailing list spool imports

api/lists/patchset.go
@@ -1,0 +1,268 @@
+package lists
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"regexp"
+	"strconv"
+	"strings"
+
+	"git.sr.ht/~sircmpwn/lists.sr.ht/api/graph/model"
+	"github.com/bluekeyes/go-gitdiff/gitdiff"
+)
+
+func identifyPatch(body string) bool {
+	body = strings.ReplaceAll(body, "\r\n", "\n")
+	files, _, err := gitdiff.Parse(strings.NewReader(body))
+	if err != nil {
+		return false
+	}
+	return len(files) > 0
+}
+
+var (
+	patchSubjectRE = regexp.MustCompile(
+		`^.*\[(?:RFC )?PATCH(?: (?P<prefix>[^\]]+))?\] (?P<subject>.*)$`,
+	)
+	patchVersionRE = regexp.MustCompile(
+		`(?:v(?P<version>[0-9]+))?(?: ?(?P<index>[0-9]+)/(?P<count>[0-9]+))?$`,
+	)
+)
+
+type PatchDetails struct {
+	Prefix  string
+	Subject string
+	Version int
+	Index   int
+	Count   int
+}
+
+func parsePatchSubject(subject string) (*PatchDetails, error) {
+	var patch PatchDetails
+
+	submatch := patchSubjectRE.FindStringSubmatch(subject)
+	if submatch == nil {
+		// TODO: figure out a better way of dealing with patches that have weird
+		// subjects
+		return nil, nil
+	}
+
+	patch.Prefix = submatch[patchSubjectRE.SubexpIndex("prefix")]
+	patch.Subject = submatch[patchSubjectRE.SubexpIndex("subject")]
+	patch.Version = 1
+	patch.Index = 1
+	patch.Count = 1
+
+	submatch = patchVersionRE.FindStringSubmatch(patch.Prefix)
+	if submatch != nil {
+		patch.Prefix = strings.TrimSuffix(patch.Prefix, submatch[0])
+
+		var err error
+		versionMatch := submatch[patchVersionRE.SubexpIndex("version")]
+		if len(versionMatch) != 0 {
+			patch.Version, err = strconv.Atoi(versionMatch)
+			if err != nil {
+				return nil, err
+			}
+		}
+		indexMatch := submatch[patchVersionRE.SubexpIndex("index")]
+		if len(indexMatch) != 0 {
+			patch.Index, err = strconv.Atoi(indexMatch)
+			if err != nil {
+				return nil, err
+			}
+		}
+		countMatch := submatch[patchVersionRE.SubexpIndex("count")]
+		if len(countMatch) != 0 {
+			patch.Count, err = strconv.Atoi(countMatch)
+			if err != nil {
+				return nil, err
+			}
+		}
+	}
+
+	return &patch, nil
+}
+
+func importPatch(tx *sql.Tx, listID int, emailID int32, threadID int32, subject string, isPatch bool) error {
+	patch, err := parsePatchSubject(subject)
+	if err != nil {
+		return fmt.Errorf("Error parsing patch subject: %v", err)
+	} else if patch == nil {
+		return nil
+	}
+
+	// Consider cover letters (index = 0) as valid patches. This makes it much
+	// easier to look up the cover letter later on by querying patch_index = 0.
+	// This also handles the case where the cover letter is received last.
+	if !isPatch && patch.Index != 0 {
+		return nil
+	}
+
+	if _, err := tx.Exec(
+		`UPDATE email SET
+			patch_index = $1, patch_count = $2, patch_version = $3,
+			patch_prefix = $4, patch_subject = $5
+		WHERE id = $6`,
+		patch.Index, patch.Count, patch.Version, patch.Prefix, patch.Subject,
+		emailID,
+	); err != nil {
+		return err
+	}
+
+	log.Printf("Received patch %d/%d: %q", patch.Index, patch.Count, patch.Subject)
+
+	// Check if the patchset is complete
+	complete := true
+	for i := 1; i <= patch.Count; i++ {
+		var exists bool
+		row := tx.QueryRow(
+			`SELECT EXISTS (
+				SELECT FROM email
+				WHERE (id = $1 OR thread_id = $1) AND patch_index = $2
+			)`,
+			threadID, i,
+		)
+		if err := row.Scan(&exists); err != nil {
+			return err
+		}
+		if !exists {
+			complete = false
+			break
+		}
+	}
+	if !complete {
+		return nil
+	}
+	log.Println("Complete patchset received")
+
+	// Look for existing patchset
+	var existing bool
+	row := tx.QueryRow(
+		`SELECT EXISTS (
+			SELECT FROM email
+			WHERE (id = $1 OR thread_id = $1) AND patchset_id IS NOT NULL
+		)`,
+		threadID,
+	)
+	if err := row.Scan(&existing); err != nil {
+		if err != sql.ErrNoRows {
+			return err
+		}
+	} else if existing {
+		// TODO: is this a new revision? complicated
+		return nil
+	}
+
+	// Look for a cover letter
+	var coverLetterID *int
+	var patchsetSubject string
+	var patchsetPrefix string
+	var patchsetVersion int
+	row = tx.QueryRow(
+		`SELECT
+			id, patch_subject, patch_prefix, patch_version
+		FROM email WHERE (id = $1 OR thread_id = $1) AND patch_index = 0`,
+		threadID,
+	)
+	if err := row.Scan(&coverLetterID,
+		&patchsetSubject, &patchsetPrefix, &patchsetVersion); err != nil {
+		if err != sql.ErrNoRows {
+			return err
+		}
+	}
+
+	if coverLetterID == nil {
+		// TODO: handle the case where patch subjects/prefixes/versions/senders
+		// don't match?
+		patchsetSubject = patch.Subject
+		patchsetPrefix = patch.Prefix
+		patchsetVersion = patch.Version
+	}
+
+	// Get the info need to reply to the last message in the patchset.
+	// This info is exposed via the lists.sr.ht REST API and is used by
+	// hub.sr.ht to automatically reply to patches.
+	var messageID string
+	var submitter sql.NullString
+	var replyTo sql.NullString
+	row = tx.QueryRow(
+		`SELECT
+			message_id, headers -> 'From' -> 0, headers -> 'Reply-To' -> 0
+		FROM email WHERE (id = $1 OR thread_id = $1) AND patch_index = $2`,
+		threadID, patch.Count,
+	)
+	if err := row.Scan(&messageID, &submitter, &replyTo); err != nil {
+		return err
+	}
+
+	var patchsetID int
+	row = tx.QueryRow(`
+		INSERT INTO patchset (
+			created, updated,
+			subject, prefix, version, list_id, cover_letter_id,
+			message_id, submitter, reply_to
+		) VALUES (
+			NOW() at time zone 'utc',
+			NOW() at time zone 'utc',
+			$1, $2, $3, $4, $5, $6, $7, $8
+		) RETURNING id`,
+		patchsetSubject, patchsetPrefix, patchsetVersion, listID, coverLetterID,
+		messageID, submitter, replyTo,
+	)
+	if err := row.Scan(&patchsetID); err != nil {
+		return err
+	}
+
+	if _, err := tx.Exec(
+		`UPDATE email SET patchset_id = $1 WHERE id = $2 OR thread_id = $2`,
+		patchsetID, threadID,
+	); err != nil {
+		return err
+	}
+
+	// TODO: Legacy webhooks
+
+	// TODO: identify patchset that this supersedes, if appropriate
+
+	return nil
+}
+
+// Update the status of a patchset
+func updatePatchsetStatus(tx *sql.Tx, threadID int32, senderID int, senderEmail string, status string) error {
+	// TODO: use a postgresql enum
+	if !model.PatchsetStatus(strings.ToUpper(status)).IsValid() {
+		return nil
+	}
+
+	row := tx.QueryRow(`
+		SELECT COALESCE(
+			access.permissions,
+			CASE WHEN list.owner_id = $2
+				THEN $4
+				ELSE list.default_access
+			END)
+		FROM email
+		JOIN list ON list.id = email.list_id
+		LEFT JOIN access ON access.list_id = list.id
+			AND (access.user_id = $2 OR access.email = $3)
+		WHERE email.id = $1`,
+		threadID, senderID, senderEmail, model.ACCESS_ALL,
+	)
+
+	var access sql.NullInt32
+	if err := row.Scan(&access); err != nil {
+		return err
+	}
+	if access.Valid && access.Int32&model.ACCESS_MODERATE != 0 {
+		if _, err := tx.Exec(`
+			UPDATE patchset SET status = $2
+			WHERE id = (SELECT patchset_id FROM email WHERE id = $1)`,
+			threadID, status,
+		); err != nil {
+			return err
+		}
+	}
+	return nil
+}

api/go.mod
@@ -5,11 +5,13 @@ go 1.16
 require (
 	git.sr.ht/~emersion/go-emailthreads v0.0.0-20220426123715-fd44f472f510
 	git.sr.ht/~sircmpwn/core-go v0.0.0-20220922085005-bb33e3e74975
+	git.sr.ht/~sircmpwn/dowork v0.0.0-20210820133136-d3970e97def3
 	git.sr.ht/~sircmpwn/getopt v1.0.0 // indirect
 	git.sr.ht/~sircmpwn/go-bare v0.0.0-20210406120253-ab86bc2846d9 // indirect
 	github.com/99designs/gqlgen v0.17.20
 	github.com/Masterminds/squirrel v1.5.2
 	github.com/ProtonMail/go-crypto v0.0.0-20220113124808-70ae35bab23f // indirect
+	github.com/bluekeyes/go-gitdiff v0.6.1
 	github.com/emersion/go-mbox v1.0.2
 	github.com/emersion/go-message v0.15.0
 	github.com/emersion/go-sasl v0.0.0-20211008083017-0b9dcfb154ac // indirect

api/go.sum
@@ -74,6 +74,8 @@ github.com/beorn7/perks v0.0.0-20180321164747-3a771d992973/go.mod h1:Dwedo/Wpr24
 github.com/beorn7/perks v1.0.0/go.mod h1:KWe93zE9D1o94FZ5RNwFwVgaQK1VOXiVxmqh+CedLV8=
 github.com/beorn7/perks v1.0.1 h1:VlbKKnNfV8bJzeqoa4cOKqO6bYr3WgKZxO8Z16+hsOM=
 github.com/beorn7/perks v1.0.1/go.mod h1:G2ZrVWU2WbWT9wwq4/hrbKbnv/1ERSJQ0ibhJ6rlkpw=
+github.com/bluekeyes/go-gitdiff v0.6.1 h1:VPpQZanEVwFid0IoAIyiJLD+SMWM/AXZXYFc+3E9CS4=
+github.com/bluekeyes/go-gitdiff v0.6.1/go.mod h1:QpfYYO1E0fTVHVZAZKiRjtSGY9823iCdvGXBcEzHGbM=
 github.com/census-instrumentation/opencensus-proto v0.2.1/go.mod h1:f6KPmirojxKA12rnyqOA5BBL4O983OfeGPqjHWSTneU=
 github.com/cespare/xxhash/v2 v2.1.1/go.mod h1:VGX0DQ3Q6kWi7AoAeZDth3/j3BFtOZR5XLFGgcrjCOs=
 github.com/cespare/xxhash/v2 v2.1.2 h1:YRXhKfTDauu4ajMg1TPgFO5jnlC2HCbmLXMcTG5cbYE=

api/graph/schema.graphqls
@@ -13,6 +13,7 @@ length of time; applications should submit a new GraphQL query each time they
 wish to access the data at the provided URL.
 """
 scalar URL
+scalar Upload
 
 "Used to provide a human-friendly description of an access scope"
 directive @scopehelp(details: String!) on ENUM_VALUE
@@ -732,6 +733,9 @@ type Mutation {
 
   "Deletes a mailing list subscription"
   mailingListUnsubscribe(listID: Int!): MailingListSubscription @access(scope: SUBSCRIPTIONS, kind: RW)
+
+  "Imports a mail spool (must be in the Mbox format)"
+  importMailingListSpool(listID: Int!, spool: Upload!): Boolean! @access(scope: LISTS, kind: RW)
 
   """
   Creates a new user webhook subscription. When an event from the

api/graph/schema.resolvers.go
@@ -9,6 +9,7 @@ import (
 	"database/sql"
 	"errors"
 	"fmt"
+	"io"
 	"net/url"
 	"strings"
 	"time"
@@ -23,8 +24,10 @@ import (
 	corewebhooks "git.sr.ht/~sircmpwn/core-go/webhooks"
 	"git.sr.ht/~sircmpwn/lists.sr.ht/api/graph/api"
 	"git.sr.ht/~sircmpwn/lists.sr.ht/api/graph/model"
+	"git.sr.ht/~sircmpwn/lists.sr.ht/api/lists"
 	"git.sr.ht/~sircmpwn/lists.sr.ht/api/loaders"
 	"git.sr.ht/~sircmpwn/lists.sr.ht/api/webhooks"
+	"github.com/99designs/gqlgen/graphql"
 	sq "github.com/Masterminds/squirrel"
 	_ "github.com/emersion/go-message/charset"
 	"github.com/emersion/go-message/mail"
@@ -1032,6 +1035,31 @@ func (r *mutationResolver) MailingListUnsubscribe(ctx context.Context, listID in
 		return nil, err
 	}
 	return &sub, nil
+}
+
+// ImportMailingListSpool is the resolver for the importMailingListSpool field.
+func (r *mutationResolver) ImportMailingListSpool(ctx context.Context, listID int, spool graphql.Upload) (bool, error) {
+	const limit = 104857600 // 100 MiB
+	if spool.Size > limit {
+		return false, errors.New("Mailing list spool must not exceed 30 MiB in size")
+	}
+	if err := database.WithTx(ctx, nil, func(tx *sql.Tx) error {
+		_, err := tx.ExecContext(ctx, `
+			UPDATE list
+			SET import_in_progress = true
+			WHERE id = $1 AND owner_id = $2
+		`, listID, auth.ForContext(ctx).UserID)
+		return err
+	}); err != nil {
+		return false, err
+	}
+
+	b, err := io.ReadAll(io.LimitReader(spool.File, limit))
+	if err != nil {
+		return false, err
+	}
+	lists.ImportMailingListSpool(ctx, listID, bytes.NewReader(b))
+	return true, nil
 }
 
 // CreateUserWebhook is the resolver for the createUserWebhook field.

api/lists/middleware.go
@@ -1,0 +1,68 @@
+package lists
+
+import (
+	"context"
+	"database/sql"
+	"io"
+	"log"
+	"net/http"
+	"time"
+
+	"git.sr.ht/~sircmpwn/core-go/database"
+	work "git.sr.ht/~sircmpwn/dowork"
+)
+
+type contextKey struct {
+	name string
+}
+
+var ctxKey = &contextKey{"lists"}
+
+func Middleware(queue *work.Queue) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			ctx := context.WithValue(r.Context(), ctxKey, queue)
+			r = r.WithContext(ctx)
+			next.ServeHTTP(w, r)
+		})
+	}
+}
+
+// Schedules a mailing list import.
+func ImportMailingListSpool(ctx context.Context, listID int, spool io.Reader) {
+	queue, ok := ctx.Value(ctxKey).(*work.Queue)
+	if !ok {
+		panic("No lists worker for this context")
+	}
+	task := work.NewTask(func(ctx context.Context) error {
+		importCtx, cancel := context.WithTimeout(ctx, 30*time.Minute)
+		defer cancel()
+
+		defer func() {
+			r := recover()
+
+			if err := database.WithTx(ctx, nil, func(tx *sql.Tx) error {
+				_, err := tx.ExecContext(ctx, `
+				UPDATE list
+				SET import_in_progress = false
+				WHERE id = $1
+			`, listID)
+				return err
+			}); err != nil {
+				panic(err)
+			}
+
+			if r != nil {
+				panic(r)
+			}
+		}()
+
+		err := importMailingListSpool(importCtx, listID, spool)
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+	queue.Enqueue(task)
+	log.Printf("Enqueued mail spool import for mailing list %d", listID)
+}

api/lists/process.go
@@ -1,0 +1,347 @@
+package lists
+
+import (
+	"bytes"
+	"context"
+	"database/sql"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"strings"
+
+	"git.sr.ht/~sircmpwn/core-go/database"
+	"github.com/emersion/go-mbox"
+	"github.com/emersion/go-message/mail"
+	"github.com/lib/pq"
+)
+
+func importMailingListSpool(ctx context.Context, listID int, spool io.Reader) error {
+	r := mbox.NewReader(spool)
+	if err := database.WithTx(ctx, nil, func(tx *sql.Tx) error {
+		for {
+			select {
+			case <-ctx.Done():
+				return errors.New("Mailing list spool import timed out")
+			default:
+			}
+
+			msg, err := r.NextMessage()
+			if err == io.EOF {
+				break
+			} else if err != nil {
+				return fmt.Errorf("Error reading mailing list spool: %v", err)
+			}
+
+			if err := archiveMessage(tx, listID, msg, true); err != nil {
+				// TODO: Collect errors and email them to the user
+				log.Println("Error importing message: %v", err)
+				continue
+			}
+		}
+		return nil
+	}); err != nil {
+		return err
+	}
+	return nil
+}
+
+func archiveMessage(tx *sql.Tx, listID int, r io.Reader, isImported bool) error {
+	var envelope bytes.Buffer
+
+	mr, err := mail.CreateReader(io.TeeReader(r, &envelope))
+	if err != nil {
+		return err
+	}
+	subject, err := mr.Header.Subject()
+	if err != nil {
+		return fmt.Errorf("Error reading Subject: %w", err)
+	}
+	messageID, err := mr.Header.MessageID()
+	if err != nil {
+		return fmt.Errorf("Error reading Message-ID: %w", err)
+	}
+	// TODO: Store Message-ID without "<>" in database
+	messageID = "<" + messageID + ">"
+	date, err := mr.Header.Date()
+	if err != nil {
+		return fmt.Errorf("Error reading Date: %w", err)
+	}
+	inReplyToList, err := mr.Header.MsgIDList("In-Reply-To")
+	if err != nil {
+		return fmt.Errorf("Error reading In-Reply-To: %w", err)
+	}
+	var inReplyTo sql.NullString
+	if len(inReplyToList) > 0 {
+		// TODO: multiple In-Reply-To message IDs?
+		inReplyTo.String = inReplyToList[0]
+		inReplyTo.Valid = true
+	}
+
+	var body string
+
+	for {
+		p, err := mr.NextPart()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			return fmt.Errorf("Error reading message part: %w", err)
+		}
+
+		switch p.Header.(type) {
+		case *mail.InlineHeader:
+			b, _ := ioutil.ReadAll(p.Body)
+			body = string(b)
+			// TODO: multiple text parts?
+			break
+		case *mail.AttachmentHeader:
+			// Do nothing
+		}
+	}
+
+	isPatch := identifyPatch(body)
+	// TODO: Identify request-pull
+	isRequestPull := false
+
+	headerMap, err := json.Marshal(mr.Header.Map())
+	if err != nil {
+		return err
+	}
+
+	var exists bool
+	row := tx.QueryRow(`
+		SELECT EXISTS(
+			SELECT FROM email WHERE list_id = $1 AND message_id = $2
+		)`,
+		listID, messageID,
+	)
+	if err := row.Scan(&exists); err != nil {
+		return err
+	}
+	if exists {
+		// Skip this message
+		return fmt.Errorf("Skipping duplicate message %q", messageID)
+	}
+
+	var emailID int32
+	row = tx.QueryRow(`
+		INSERT INTO email (
+			created, updated, subject, message_id, message_date,
+			envelope, headers, body,
+			list_id, parent_id, thread_id, sender_id,
+			is_patch, is_request_pull,
+			nreplies,
+			nparticipants,
+			in_reply_to,
+			patchset_id,
+			patch_index,
+			patch_count,
+			patch_version,
+			patch_prefix,
+			patch_subject,
+			superseded_by_id
+		) VALUES (
+			CASE WHEN $1 THEN $2 ELSE NOW() at time zone 'utc' END,
+			CASE WHEN $1 THEN $2 ELSE NOW() at time zone 'utc' END,
+			$3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13,
+			$14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24
+		) RETURNING id`,
+		isImported, date,
+		subject, messageID, date,
+		envelope.String(), string(headerMap), body,
+		listID, nil, nil, nil,
+		isPatch, isRequestPull,
+		0, 1, inReplyTo,
+		nil, nil, nil, nil, nil, nil, nil,
+	)
+	if err := row.Scan(&emailID); err != nil {
+		return err
+	}
+
+	// Set parent of this email
+	var parentID int
+	row = tx.QueryRow(
+		`SELECT id FROM email WHERE list_id = $1 AND message_id = $2;`,
+		listID, "<"+inReplyTo.String+">",
+	)
+	if err := row.Scan(&parentID); err != nil {
+		if err != sql.ErrNoRows {
+			return err
+		}
+	} else {
+		if _, err := tx.Exec(
+			`UPDATE email SET parent_id = $1 WHERE id = $2`,
+			parentID, emailID,
+		); err != nil {
+			return err
+		}
+	}
+
+	threadID, err := computeThreadID(tx, emailID)
+	if err != nil {
+		return err
+	}
+	if threadID != emailID {
+		if _, err := tx.Exec(
+			`UPDATE email SET thread_id = $1 WHERE id = $2`,
+			threadID, emailID,
+		); err != nil {
+			return err
+		}
+	}
+
+	if err := reparentEmails(tx, listID, threadID, emailID, messageID); err != nil {
+		return err
+	}
+	if err := updateThreadReplies(tx, threadID); err != nil {
+		return err
+	}
+
+	// TODO: Enumerate CC's and create SQL relationships for them
+	// TODO: Some users will have many email addresses
+	// TODO: Mutliple From addresses?
+	senders, err := mr.Header.AddressList("From")
+	if err != nil {
+		return fmt.Errorf("Error reading From: %q %w", mr.Header.Get("From"), err)
+	}
+	if len(senders) == 0 {
+		return errors.New("expected at least one From address")
+	}
+	// Lookup sender by email
+	row = tx.QueryRow(
+		`SELECT id FROM "user" WHERE email = $1`,
+		senders[0].Address,
+	)
+	var senderID int
+	if err := row.Scan(&senderID); err != nil {
+		if err != sql.ErrNoRows {
+			return err
+		}
+	} else {
+		if _, err := tx.Exec(
+			`UPDATE email SET sender_id = $1 WHERE id = $2`,
+			senderID, emailID,
+		); err != nil {
+			return err
+		}
+	}
+
+	if err := importPatch(tx, listID, emailID, threadID, subject, isPatch); err != nil {
+		return err
+	}
+
+	// Update patchset status
+	const updateHeader = "X-Sourcehut-Patchset-Update"
+	if !isPatch && mr.Header.Has(updateHeader) {
+		status := strings.ToLower(mr.Header.Get(updateHeader))
+		if err := updatePatchsetStatus(tx, threadID, senderID, senders[0].Address, status); err != nil {
+			return err
+		}
+	}
+
+	log.Printf("Archived message %q", messageID)
+
+	return nil
+}
+
+// Computes the thread ID for the given email
+func computeThreadID(tx *sql.Tx, emailID int32) (int32, error) {
+	// Keep track of seen emails to avoid reference loops
+	threadID := emailID
+	seen := map[int32]struct{}{}
+	for {
+		if _, ok := seen[threadID]; ok {
+			// Reference loop
+			break
+		}
+		seen[threadID] = struct{}{}
+		row := tx.QueryRow(
+			`SELECT parent_id FROM email WHERE id = $1`,
+			threadID,
+		)
+		var nextID *int32
+		if err := row.Scan(&nextID); err != nil {
+			return 0, err
+		}
+		if nextID == nil {
+			break
+		}
+		threadID = *nextID
+	}
+	return threadID, nil
+}
+
+// Reparent emails that arrived out-of-order
+func reparentEmails(tx *sql.Tx, listID int, threadID, emailID int32, messageID string) error {
+	children, err := tx.Query(
+		`SELECT id, thread_id FROM email WHERE list_id = $1 AND in_reply_to = $2`,
+		listID, messageID,
+	)
+	if err != nil {
+		return err
+	}
+	defer children.Close()
+	var childIDs []int32
+	var oldThreadIDs []int32
+	for children.Next() {
+		var childID int32
+		var childThreadID *int32
+		if err := children.Scan(&childID, &childThreadID); err != nil {
+			return err
+		}
+		childIDs = append(childIDs, childID)
+		if childThreadID == nil {
+			oldThreadIDs = append(oldThreadIDs, childID)
+		} else if *childThreadID != threadID {
+			oldThreadIDs = append(oldThreadIDs, *childThreadID)
+		}
+	}
+	if _, err := tx.Exec(
+		`UPDATE email SET parent_id = $1, thread_id = $2 WHERE id = ANY($3)`,
+		emailID, threadID, pq.Int32Array(childIDs),
+	); err != nil {
+		return err
+	}
+	if _, err := tx.Exec(
+		`UPDATE email SET thread_id = $1 WHERE thread_id = ANY($2)`,
+		threadID, pq.Int32Array(oldThreadIDs),
+	); err != nil {
+		return err
+	}
+	return nil
+}
+
+// Updates thread nreplies and nparticipants
+func updateThreadReplies(tx *sql.Tx, threadID int32) error {
+	nreplies := 0
+	memberIDs := []int32{threadID}
+	participants := make(map[string]struct{})
+	threadMembers, err := tx.Query(
+		`SELECT id, headers -> 'From' -> 0 FROM email WHERE thread_id = $1`,
+		threadID,
+	)
+	if err != nil {
+		return err
+	}
+	defer threadMembers.Close()
+	for threadMembers.Next() {
+		var memberID int32
+		var fromHeader string
+		if err := threadMembers.Scan(&memberID, &fromHeader); err != nil {
+			return err
+		}
+		memberIDs = append(memberIDs, memberID)
+		// TODO: multiple From addresses?
+		participants[fromHeader] = struct{}{}
+		nreplies++
+	}
+	if _, err := tx.Exec(
+		`UPDATE email SET nreplies = $1, nparticipants = $2 WHERE id = $3`,
+		nreplies, len(participants), threadID,
+	); err != nil {
+		return err
+	}
+	return nil
+}

api/server.go
@@ -13,6 +13,7 @@ import (
 	"git.sr.ht/~sircmpwn/core-go/database"
 	"git.sr.ht/~sircmpwn/core-go/server"
 	"git.sr.ht/~sircmpwn/core-go/webhooks"
+	work "git.sr.ht/~sircmpwn/dowork"
 	"github.com/99designs/gqlgen/graphql"
 	"github.com/emersion/go-mbox"
 	_ "github.com/emersion/go-message/charset"
@@ -22,6 +23,7 @@ import (
 	"git.sr.ht/~sircmpwn/lists.sr.ht/api/graph"
 	"git.sr.ht/~sircmpwn/lists.sr.ht/api/graph/api"
 	"git.sr.ht/~sircmpwn/lists.sr.ht/api/graph/model"
+	"git.sr.ht/~sircmpwn/lists.sr.ht/api/lists"
 	"git.sr.ht/~sircmpwn/lists.sr.ht/api/loaders"
 )
 
@@ -43,6 +45,7 @@ func main() {
 		scopes[i] = s.String()
 	}
 
+	listsQueue := work.NewQueue("lists")
 	webhookQueue := webhooks.NewQueue(schema)
 	legacyWebhooks := webhooks.NewLegacyQueue()
 
@@ -50,10 +53,11 @@ func main() {
 		WithDefaultMiddleware().
 		WithMiddleware(
 			loaders.Middleware,
+			lists.Middleware(listsQueue),
 			webhooks.Middleware(webhookQueue),
 			webhooks.LegacyMiddleware(legacyWebhooks),
 		).
-		WithQueues(webhookQueue.Queue, legacyWebhooks.Queue).
+		WithQueues(listsQueue, webhookQueue.Queue, legacyWebhooks.Queue).
 		WithSchema(schema, scopes)
 
 	// Bulk transfer endpoints

commit 189eccd78d2d667bcd779cc27c7a18a237254324

api: remove trailing spaces from patchset prefixes
When processing a message that has the following subject:

	[PATCH foo v2 1/8] blah: blah blah

We get the following result (note the trailing space at the end of
Prefix):

	PatchDetails{
		Prefix: "foo ",
		Subject: "blah: blah blah"
		Version: 2,
		Index: 1,
		Count: 8,
	}

Trim spaces from the prefix when it has a version and/or patch
number/count suffix.

Fixes: cde342868e5d ("api: Implement mailing list spool imports")
Signed-off-by: Robin Jarry <robin@jarry.cc>

api/lists/patchset.go
@@ -57,7 +57,8 @@ func parsePatchSubject(subject string) (*PatchDetails, error) {
 
 	submatch = patchVersionRE.FindStringSubmatch(patch.Prefix)
 	if submatch != nil {
-		patch.Prefix = strings.TrimSuffix(patch.Prefix, submatch[0])
+		patch.Prefix = strings.TrimSpace(
+			strings.TrimSuffix(patch.Prefix, submatch[0]))
 
 		var err error
 		versionMatch := submatch[patchVersionRE.SubexpIndex("version")]