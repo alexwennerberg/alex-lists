alexwennerberg/alex-lists

commit 82638fc6a9b7eae0b91922f9d9f073416cb51bb0

ingress: fix message archival and forwarding
Once created, a message.Entity can only be consumed once. During the
message validation, we walk through all parts to check if there is
a text/plain part and no forbidden MIME types. This effectively consumes
the reader and causes subsequent msg.WriteTo() to only write the message
headers without any body content.

This is silently accepted by all parsers because an empty email with
a single inline text/plain part is valid. However, when dealing with
multipart messages, we get errors from the message parser:

   Error reading message part: multipart: NextPart: EOF

Avoid reusing message.Entity objects. Instead pass down a bytes array to
ProcessMessage() and directly archive it.

Fixes: d162fddce831 ("Reimplement the ingress daemon using go")
Signed-off-by: Robin Jarry <robin@jarry.cc>

ingress/backend.go
@@ -84,13 +84,14 @@ func (s *Session) Data(r io.Reader) error {
 		from  string
 		to    []string
 		err   error
-		data  bytes.Buffer
+		buf   bytes.Buffer
+		data  []byte
 		n     int64
 	)
 
 	EmailsCounter.Inc()
 
-	n, err = io.CopyN(&data, r, Config.MaxMessageSize)
+	n, err = io.CopyN(&buf, r, Config.MaxMessageSize)
 	switch {
 	case n == Config.MaxMessageSize:
 		err = fmt.Errorf("Message too big.")
@@ -104,10 +105,13 @@ func (s *Session) Data(r io.Reader) error {
 		goto end
 	}
 
-	email, err = message.Read(&data)
-	if err != nil {
+	data = buf.Bytes()
+
+	email, err = message.Read(bytes.NewReader(data))
+	if err != nil && message.IsUnknownCharset(err) {
 		goto end
 	}
+
 	switch strings.ToLower(email.Header.Get("Auto-Submitted")) {
 	case "auto-generated", "auto-replied":
 		// disregard automatic emails like OOO replies
@@ -123,7 +127,7 @@ func (s *Session) Data(r io.Reader) error {
 	}
 
 	log.Printf(
-		" message received from=%s to=%s message_id=%s subject=%s",
+		"message received from=%s to=%s message_id=%s subject=%s",
 		s.from,
 		strings.Join(s.to, ","),
 		email.Header.Get("Message-Id"),
@@ -137,7 +141,7 @@ func (s *Session) Data(r io.Reader) error {
 
 	// Defer processing to the ingress worker.
 	s.backend.ingress.Enqueue(work.NewTask(func(context.Context) error {
-		return s.backend.ProcessMessage(from, to, email)
+		return s.backend.ProcessMessage(from, to, data)
 	}).Retries(10))
 
 end:
@@ -150,14 +154,18 @@ end:
 }
 
 func (b *Backend) ProcessMessage(
-	from string, recipients []string, email *message.Entity,
+	from string, recipients []string, data []byte,
 ) error {
 	var (
+		email  *message.Entity
 		list   *MailingList
 		sender *Sender
 		failed int
 		err    error
 	)
+
+	// cannot fail, we already parsed it once in Session.Data()
+	email, _ = message.Read(bytes.NewReader(data))
 
 	for _, to := range recipients {
 		sender, list, err = LookupEmailDetails(email, to)
@@ -178,7 +186,7 @@ func (b *Backend) ProcessMessage(
 			CommandsCounter.Inc()
 			err = b.ConfirmUnsubscribe(sender, email, list)
 		default:
-			err = b.Post(sender, email, list)
+			err = b.Post(sender, data, email, list)
 		}
 	next:
 		if _, ok := err.(BounceError); ok {

ingress/api.go
@@ -215,12 +215,7 @@ func ConfirmUnsubscription(sender *Sender, list *MailingList, token string) erro
 	)
 }
 
-func ArchiveMessage(msg *message.Entity, list *MailingList) error {
-	var buf bytes.Buffer
-	if err := msg.WriteTo(&buf); err != nil {
-		return err
-	}
-
+func ArchiveMessage(data []byte, list *MailingList) error {
 	ctx := config.Context(context.TODO(), SrhtConfig, LISTS_SERVICE)
 	return client.Do(
 		ctx, list.Owner, LISTS_SERVICE,
@@ -232,7 +227,7 @@ func ArchiveMessage(msg *message.Entity, list *MailingList) error {
 			Uploads: map[string]graphql.Upload{
 				"msg": {
 					Filename:    "archive",
-					File:        bytes.NewReader(buf.Bytes()),
+					File:        bytes.NewReader(data),
 					ContentType: "message/rfc822",
 				},
 			},

ingress/post.go
@@ -4,6 +4,7 @@
 package main
 
 import (
+	"bytes"
 	"errors"
 	"fmt"
 	"log"
@@ -16,7 +17,7 @@ import (
 	"github.com/emersion/go-message/mail"
 )
 
-func (b *Backend) Post(sender *Sender, msg *message.Entity, list *MailingList) error {
+func (b *Backend) Post(sender *Sender, data []byte, msg *message.Entity, list *MailingList) error {
 	if !(sender.ACL.Post || (list.IsReply && sender.ACL.Reply)) {
 		return &PostPermError{sender, list}
 	}
@@ -27,12 +28,13 @@ func (b *Backend) Post(sender *Sender, msg *message.Entity, list *MailingList) e
 
 	msgID := msg.Header.Get("Message-ID")
 
-	err := ArchiveMessage(msg, list)
+	// Validate() consumed the message.Entity body, directly archive raw data
+	err := ArchiveMessage(data, list)
 
 	switch {
 	case err == nil:
 		log.Printf("Archived %s to %s", msgID, list.FullName())
-		return b.ForwardMessage(msg, list)
+		return b.ForwardMessage(data, list)
 
 	case errors.Is(err, apierr.ErrDuplicateEmail):
 		log.Printf("Dropping duplicate message %s on %s", msgID, list.FullName())
@@ -52,7 +54,7 @@ var reservedHeaders = []string{
 	"Sender",
 }
 
-func (b *Backend) ForwardMessage(msg *message.Entity, list *MailingList) error {
+func (b *Backend) ForwardMessage(data []byte, list *MailingList) error {
 	var recipients []string
 	alreadyCopied := make(map[string]bool)
 
@@ -61,6 +63,9 @@ func (b *Backend) ForwardMessage(msg *message.Entity, list *MailingList) error {
 	if err != nil {
 		return err
 	}
+
+	// cannot fail, the message has already been validated
+	msg, _ := message.Read(bytes.NewReader(data))
 
 	// eliminate recipients that were already included in the original message
 	header := mail.Header{Header: msg.Header}

commit b21eb8847384cb09b52c2b1ffc0ae62edb823eec

ingress: do not reject messages with unknown body charsets
Fix the logic here. We want to *ignore* UnknownCharsetErrors not the
opposite.

Fixes: 82638fc6a9b7 ("ingress: fix message archival and forwarding")
Signed-off-by: Robin Jarry <robin@jarry.cc>

ingress/backend.go
@@ -108,7 +108,10 @@ func (s *Session) Data(r io.Reader) error {
 	data = buf.Bytes()
 
 	email, err = message.Read(bytes.NewReader(data))
-	if err != nil && message.IsUnknownCharset(err) {
+	if err != nil && !message.IsUnknownCharset(err) {
+		// We may get a non-fatal UnknownCharsetError, but that does
+		// not mean we should drop the message. Only drop it if parsing
+		// completely failed.
 		goto end
 	}
 