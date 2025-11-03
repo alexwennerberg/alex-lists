67a494afc41c350d1bc64934728dac1a2f38a452
Conrad Hoffmann <ch@bitfehler.net>
2025-11-03 12:28:54 +0100

    More indices for webhook subscriptions

--- schema.sql
+++ schema.sql
@@ -202,6 +202,7 @@ CREATE TABLE gql_user_wh_sub (
 	CONSTRAINT gql_user_wh_sub_events_check CHECK ((array_length(events, 1) > 0))
 );
 
+CREATE INDEX gql_user_wh_sub_user_id_idx ON gql_user_wh_sub USING btree (user_id);
 CREATE INDEX gql_user_wh_sub_token_hash_idx ON gql_user_wh_sub USING btree (token_hash);
 
 CREATE TABLE gql_user_wh_delivery (
@@ -242,6 +243,8 @@ CREATE TABLE gql_list_wh_sub (
 		CHECK ((array_length(events, 1) > 0))
 );
 
+CREATE INDEX gql_list_wh_sub_user_id_idx ON gql_list_wh_sub USING btree (user_id);
+CREATE INDEX gql_list_wh_sub_list_id_idx ON gql_list_wh_sub USING btree (list_id);
 CREATE INDEX gql_list_wh_sub_token_hash_idx ON gql_list_wh_sub USING btree (token_hash);
 
 CREATE TABLE gql_list_wh_delivery (
