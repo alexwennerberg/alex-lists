878be271f3b40f446c82a77b4c38be667fbf3922
Drew DeVault <drew@ddevault.org>
2025-11-04 14:32:42 +0100

    schema: drop legacy features

--- schema.sql
+++ schema.sql
@@ -40,9 +40,6 @@ CREATE TABLE "user" (
 	created timestamp without time zone NOT NULL,
 	updated timestamp without time zone NOT NULL,
 	session character varying(128),
-	oauth_token character varying(256),
-	oauth_token_expires timestamp without time zone,
-	oauth_token_scopes character varying,
 	email character varying(256) UNIQUE NOT NULL,
 	user_type user_type NOT NULL,
 	url character varying(256),
@@ -257,65 +254,4 @@ CREATE TABLE gql_list_wh_delivery (
 	response_body character varying,
 	response_headers character varying,
 	response_status integer
-);
-
--- Legacy OAuth
-CREATE TABLE oauthtoken (
-	id serial PRIMARY KEY,
-	created timestamp without time zone NOT NULL,
-	updated timestamp without time zone NOT NULL,
-	expires timestamp without time zone NOT NULL,
-	token_hash character varying(128) NOT NULL,
-	token_partial character varying(8) NOT NULL,
-	scopes character varying(512) NOT NULL,
-	user_id integer REFERENCES "user"(id) ON DELETE CASCADE
-);
-
--- Legacy webhooks
-CREATE TABLE list_webhook_subscription (
-	id serial PRIMARY KEY,
-	created timestamp without time zone NOT NULL,
-	url character varying(2048) NOT NULL,
-	events character varying NOT NULL,
-	user_id integer REFERENCES "user"(id) ON DELETE CASCADE,
-	token_id integer REFERENCES oauthtoken(id) ON DELETE CASCADE,
-	list_id integer REFERENCES list(id) ON DELETE CASCADE
-);
-
-CREATE TABLE list_webhook_delivery (
-	id serial PRIMARY KEY,
-	uuid uuid NOT NULL,
-	created timestamp without time zone NOT NULL,
-	event character varying(256) NOT NULL,
-	url character varying(2048) NOT NULL,
-	payload character varying(65536) NOT NULL,
-	payload_headers character varying(16384) NOT NULL,
-	response character varying(65536),
-	response_status integer NOT NULL,
-	response_headers character varying(16384),
-	subscription_id integer NOT NULL
-		REFERENCES list_webhook_subscription(id) ON DELETE CASCADE
-);
-
-CREATE TABLE user_webhook_subscription (
-	id serial PRIMARY KEY,
-	created timestamp without time zone NOT NULL,
-	url character varying(2048) NOT NULL,
-	events character varying NOT NULL,
-	user_id integer REFERENCES "user"(id) ON DELETE CASCADE,
-	token_id integer REFERENCES oauthtoken(id) ON DELETE CASCADE
-);
-
-CREATE TABLE user_webhook_delivery (
-	id serial PRIMARY KEY,
-	uuid uuid NOT NULL,
-	created timestamp without time zone NOT NULL,
-	event character varying(256) NOT NULL,
-	url character varying(2048) NOT NULL,
-	payload character varying(65536) NOT NULL,
-	payload_headers character varying(16384) NOT NULL,
-	response character varying(65536),
-	response_status integer NOT NULL,
-	response_headers character varying(16384),
-	subscription_id integer NOT NULL REFERENCES user_webhook_subscription(id) ON DELETE CASCADE
 );
