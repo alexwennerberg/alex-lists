alexwennerberg/alex-lists

commit d33e46ff9da171fa103c00bc0369d4e3f72dca68

lists: support requesting to receive a copy of own mails
This introduces a user level configuration flag, "off" by default, that
controls whether lists.sr.ht should send a copy of inbound emails to
their sender (whether they're subscribed to the list or not).

Fixes: https://todo.sr.ht/~sircmpwn/sourcehut/36
Signed-off-by: Simon Martin <simon@nasilyan.com>

ingress/post.go
@@ -60,16 +60,32 @@ func (b *Backend) ForwardMessage(data []byte, list *MailingList) error {
 	msg, _ := message.Read(bytes.NewReader(data))
 
 	// eliminate recipients that were already included in the original message
+	var from string
 	header := mail.Header{Header: msg.Header}
-	for _, name := range []string{"From", "To", "Cc"} {
+	for idx, name := range []string{"From", "To", "Cc"} {
 		addresses, _ := header.AddressList(name)
 		for _, addr := range addresses {
 			alreadyCopied[addr.Address] = true
+			if idx == 0 {
+				from = addr.Address
+			}
 		}
 	}
 	for _, email := range subscribers {
 		if !alreadyCopied[email] {
 			recipients = append(recipients, email)
+		}
+	}
+	copySender := CopySelf(from)
+	if copySender {
+		copied := false
+		for _, addr := range recipients {
+			if addr == from {
+				copied = true
+			}
+		}
+		if !copied {
+			recipients = append(recipients, from)
 		}
 	}
 

api/graph/model/user.go
@@ -15,6 +15,7 @@ type User struct {
 	URL      *string   `json:"url"`
 	Location *string   `json:"location"`
 	Bio      *string   `json:"bio"`
+	CopySelf bool      `json:"copy_self"`
 
 	alias  string
 	fields *database.ModelFields
@@ -53,6 +54,7 @@ func (u *User) Fields() *database.ModelFields {
 			{SQL: "url", GQL: "url", Ptr: &u.URL},
 			{SQL: "location", GQL: "location", Ptr: &u.Location},
 			{SQL: "bio", GQL: "bio", Ptr: &u.Bio},
+			{SQL: "copy_self", GQL: "CopySelf", Ptr: &u.CopySelf},
 
 			// Always fetch:
 			{SQL: "id", GQL: "", Ptr: &u.ID},

api/graph/schema.graphqls
@@ -33,6 +33,12 @@ available to normal API users.
 """
 directive @internal on FIELD_DEFINITION
 
+"""
+Used to decorate fields which are for internal use, and are not available to
+normal API users.
+"""
+directive @anoninternal on FIELD_DEFINITION
+
 enum AccessScope {
   ACLS          @scopehelp(details: "access control lists")
   EMAILS        @scopehelp(details: "emails")
@@ -618,6 +624,10 @@ type WebhookSubscriptionCursor {
   cursor: Cursor
 }
 
+type Preferences {
+  copySelf: Boolean!
+}
+
 type Query {
   "Returns API version information"
   version: Version!
@@ -655,6 +665,19 @@ type Query {
   outside of a webhook context.
   """
   webhook: WebhookPayload!
+
+  """
+  Returns the preferences associated with this user account.
+  """
+  preferences: Preferences! @access(scope: PROFILE, kind: RO)
+
+  """
+  If email corresponds to a registered user, return whether they wish to
+  received their own emails; otherwise return false.
+
+  Internal use only.
+  """
+  copySelf(email: String!): Boolean! @anoninternal
 }
 
 # You may omit any fields to leave them unchanged.
@@ -689,6 +712,10 @@ input MailingListWebhookInput {
   url: String!
   events: [WebhookEvent!]!
   query: String!
+}
+
+input PreferencesInput {
+  copySelf: Boolean!
 }
 
 type Mutation {
@@ -832,4 +859,9 @@ type Mutation {
   Internal use only.
   """
   confirmUnsubscription(token: ConfirmationToken!, email: String!): MailingListSubscription! @internal
+
+  """
+  Updates the user's preferences.
+  """
+  updatePreferences(preferences: PreferencesInput!): Preferences! @access(scope: PROFILE, kind: RW)
 }

api/graph/schema.resolvers.go
@@ -1701,6 +1701,30 @@ func (r *mutationResolver) ConfirmUnsubscription(ctx context.Context, token stri
 	return &sub, nil
 }
 
+// UpdatePreferences is the resolver for the updatePreferences field.
+func (r *mutationResolver) UpdatePreferences(ctx context.Context, preferences model.PreferencesInput) (*model.Preferences, error) {
+	var copySelf bool
+	if err := database.WithTx(ctx, nil, func(tx *sql.Tx) error {
+		row := tx.QueryRowContext(ctx, `
+			UPDATE "user" SET copy_self = $1
+			WHERE id = $2
+			RETURNING copy_self;
+		`, preferences.CopySelf, auth.ForContext(ctx).UserID)
+		if err := row.Scan(&copySelf); err != nil {
+			return err
+		}
+		return nil
+	}); err != nil {
+		if err == sql.ErrNoRows {
+			copySelf = false
+		}
+		return nil, err
+	}
+	return &model.Preferences{
+		CopySelf: copySelf,
+	}, nil
+}
+
 // Submitter is the resolver for the submitter field.
 func (r *patchsetResolver) Submitter(ctx context.Context, obj *model.Patchset) (model.Entity, error) {
 	// XXX: It would be nice if we didn't have to fetch the thread details in
@@ -1916,6 +1940,50 @@ func (r *queryResolver) User(ctx context.Context, username string) (*model.User,
 	return loaders.ForContext(ctx).UsersByName.Load(username)
 }
 
+// CopySelf is the resolver for the copySelf field.
+func (r *queryResolver) CopySelf(ctx context.Context, email string) (bool, error) {
+	var copySelf bool
+	if err := database.WithTx(ctx, &sql.TxOptions{
+		Isolation: 0,
+		ReadOnly:  true,
+	}, func(tx *sql.Tx) error {
+		row := tx.QueryRowContext(ctx, `
+			SELECT copy_self FROM "user" WHERE email = $1
+		`, email)
+		if err := row.Scan(&copySelf); err != nil {
+			return err
+		}
+		return nil
+	}); err != nil {
+		if err == sql.ErrNoRows {
+			return false, nil
+		}
+	}
+	return copySelf, nil
+}
+
+func (r *mutationResolver) UpdateCopySelf(ctx context.Context, value bool) (bool, error) {
+	var user model.User
+	if err := database.WithTx(ctx, nil, func(tx *sql.Tx) error {
+		row := tx.QueryRowContext(ctx, `
+			UPDATE "user" SET copy_self = $1
+			WHERE id = $2
+			RETURNING copy_self;
+		`, value, auth.ForContext(ctx).UserID)
+		if err := row.Scan(&user.CopySelf); err != nil {
+			return err
+		}
+		return nil
+	}); err != nil {
+		if err == sql.ErrNoRows {
+			return false, nil
+		}
+		return false, err
+	}
+
+	return user.CopySelf, nil
+}
+
 // Email is the resolver for the email field.
 func (r *queryResolver) Email(ctx context.Context, id int) (*model.Email, error) {
 	return loaders.ForContext(ctx).EmailsByID.Load(id)
@@ -2025,6 +2093,26 @@ func (r *queryResolver) Webhook(ctx context.Context) (model.WebhookPayload, erro
 		panic(fmt.Errorf("Invalid webhook payload context"))
 	}
 	return payload, nil
+}
+
+// Preferences is the resolver for the preferences field.
+func (r *queryResolver) Preferences(ctx context.Context) (model.Preferences, error) {
+	var copySelf bool
+	if err := database.WithTx(ctx, nil, func(tx *sql.Tx) error {
+		row := tx.QueryRowContext(ctx, `
+			SELECT copy_self FROM "user"
+			WHERE id = $1;
+		`, auth.ForContext(ctx).UserID)
+		if err := row.Scan(&copySelf); err != nil {
+			return err
+		}
+		return nil
+	}); err != nil {
+		return nil, err
+	}
+	return &model.Preferences{
+		CopySelf: copySelf,
+	}, nil
 }
 
 // Sender is the resolver for the sender field.

api/server.go
@@ -35,6 +35,7 @@ func main() {
 
 	gqlConfig := api.Config{Resolvers: &graph.Resolver{}}
 	gqlConfig.Directives.Private = server.Private
+	gqlConfig.Directives.Anoninternal = server.AnonInternal
 	gqlConfig.Directives.Internal = server.Internal
 	gqlConfig.Directives.Access = func(ctx context.Context, obj any,
 		next graphql.Resolver, scope model.AccessScope,

ingress/api.go
@@ -278,3 +278,21 @@ func LookupSubscribers(list *MailingList) ([]string, error) {
 	}
 	return emails, nil
 }
+
+func CopySelf(address string) bool {
+	var result struct {
+		CopySelf bool `json:"copySelf"`
+	}
+	err := GqlQueryUser(
+		"",
+		`query($email: String!) {
+			copySelf(email: $email)
+		}`,
+		map[string]any{"email": address},
+		&result,
+	)
+	if err != nil {
+		return false
+	}
+	return result.CopySelf
+}

listssrht/blueprints/user.py
@@ -10,7 +10,7 @@ from srht.search import search_by
 from srht.validation import Validation
 from sqlalchemy import or_
 from sqlalchemy import nullslast
-from listssrht.graphql import Client, Visibility
+from listssrht.graphql import Client, Visibility, PreferencesInput
 from listssrht.types import List, ListAccess, User, Email, Subscription, Mirror
 import re
 import smtplib
@@ -38,8 +38,20 @@ def index():
             .filter(Subscription.user_id == current_user.id)
             .order_by(nullslast(List.last_activity.desc()))).limit(10).all()]
     notice = session.pop("notice", None)
+    client = Client()
+    copy_self = client.get_preferences().preferences.copy_self
     return render_template("dashboard.html", recent=recent,
-            subs=subs, notice=notice)
+            subs=subs, notice=notice, copy_self=copy_self)
+
+@user.route("/", methods=["POST"])
+@loginrequired
+def index_POST():
+    valid = Validation(request)
+    copy_self = valid.optional("copy-self", default=False)
+    client = Client()
+    client.update_preferences(PreferencesInput(copy_self=copy_self))
+    session["prefs_updated"] = True
+    return redirect(url_for("user.index"))
 
 @user.route("/~<username>")
 def user_profile(username):

listssrht/graphql/queries.graphql
@@ -55,3 +55,15 @@ query GetPatchsetThreadBlocks($patchset: Int!) {
     }
   }
 }
+
+query GetPreferences {
+  preferences {
+    copySelf
+  }
+}
+
+mutation UpdatePreferences($prefs: PreferencesInput!) {
+  updatePreferences(preferences: $prefs) {
+    copySelf
+  }
+}

commit 915ea12f40ff90393860fd59198fc4b78157189b

ingress: do not overly copy senders with copyself
The logic to decide whether the sender should be copied because of their
CopySelf preference was flawed, in that it did not properly check
whether the sender was also in To: or Cc: - in this case there's no need
to copy them once more.

This patch fixes this bug.

Fixes: d33e46ff9da ("lists: support requesting to receive a copy of own mails")
Reported-by: Drew DeVault <drew@ddevault.org>
Signed-off-by: Simon Martin <simon@nasilyan.com>

ingress/post.go
@@ -61,14 +61,17 @@ func (b *Backend) ForwardMessage(data []byte, list *MailingList) error {
 
 	// eliminate recipients that were already included in the original message
 	var from string
+	senderIsCopied := false
 	header := mail.Header{Header: msg.Header}
 	for idx, name := range []string{"From", "To", "Cc"} {
 		addresses, _ := header.AddressList(name)
 		for _, addr := range addresses {
-			alreadyCopied[addr.Address] = true
 			if idx == 0 {
 				from = addr.Address
+			} else if addr.Address == from {
+				senderIsCopied = true
 			}
+			alreadyCopied[addr.Address] = true
 		}
 	}
 	for _, email := range subscribers {
@@ -76,17 +79,8 @@ func (b *Backend) ForwardMessage(data []byte, list *MailingList) error {
 			recipients = append(recipients, email)
 		}
 	}
-	copySender := CopySelf(from)
-	if copySender {
-		copied := false
-		for _, addr := range recipients {
-			if addr == from {
-				copied = true
-			}
-		}
-		if !copied {
-			recipients = append(recipients, from)
-		}
+	if !senderIsCopied && CopySelf(from) {
+		recipients = append(recipients, from)
 	}
 
 	msgID := msg.Header.Get("Message-Id")